253769b87c5028abe9be1dac8ea2585fe2ab3a53
MAIN6419 <dark9737@gmail.com>
2023-09-09 00:05:08 +0900

    Feat: App 컴포넌트 webpSupport 함수 비동기 처리 및 적용 webpSupport 상태를 통해 조건부 렌더링을 주어 webp 이미지 지원 여부 확인 후 렌더링이 될 수 있도록 설정

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,41 @@
-import React from 'react'
-import { useSelector } from 'react-redux';
-import { Navigate, Route, Routes } from 'react-router-dom'
-import { RootState } from './store/store';
-import Login from './pages/login/Login';
-function App () {
+import React, { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
+import { Navigate, Route, Routes } from "react-router-dom";
+import { RootState } from "./store/store";
+import Login from "./pages/login/Login";
+import { detectWebpSupport } from "./library/webpSupport";
+function App() {
   const userData = useSelector((state: RootState) => state.user.data);
+  // webp 지원유무가 확인 되었을때 컴포넌트를 렌더링 시키위해 사용
+  const [webpChecked, setWebpChecked] = useState(false);
+
+  const checkwebp = async () => {
+    const webpSupport = await detectWebpSupport();
+    if (webpSupport) {
+      document.body.classList.add("webp");
+    } else {
+      document.body.classList.add("no-webp");
+    }
+    // webp 지원유무가 확인되었다면 true로 설정
+    setWebpChecked(true);
+  };
+
+  useEffect(() => {
+    checkwebp();
+  }, []);
+
   return (
     <>
+      {webpChecked && (
         <Routes>
-            <Route
-              path="/login"
-              element={userData ? <Navigate to="/" /> : <Login />}
-            />
+          <Route
+            path='/login'
+            element={userData ? <Navigate to='/' /> : <Login />}
+          />
         </Routes>
-      </>
-  )
+      )}
+    </>
+  );
 }
 
-export default App
+export default App;
