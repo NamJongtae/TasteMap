c22510bcda3bd64e6544a1de60a49f249013756e
MAIN6419 <dark9737@gmail.com>
2023-09-10 21:05:38 +0900

    Feat: 이미지 배열을 슬라이더로 반환해주는 ImgSlider 컴포넌트 구현

---
+++ src/compoent/units/imgSlider/ImgSlider.tsx
@@ -0,0 +1,106 @@
+import React, { useRef, useState } from "react";
+import {
+  ImgBtnList,
+  ImgItem,
+  ImgList,
+  ImgWrapper,
+  ImgBtnItem,
+  ImgBtn,
+  PrevBtn,
+  NextBtn
+} from "./imgSlider.styles";
+import ProgressiveImg from "../../commons/progressiveImg/ProgressiveImg";
+
+interface IProps {
+  imgArray: string[];
+}
+export default function ImgSlider({ imgArray }: IProps) {
+  const [activeButton, setActiveButton] = useState(0);
+  const ImgUlRef = useRef<HTMLUListElement>(null);
+  // 이미지 슬라이드를 위해 해당 이미지의 이미지 크기에 인덱스를 곱해 translateX 해줌
+  const onClickSliderBtn = (idx: number) => {
+    if (ImgUlRef.current) {
+      ImgUlRef.current.style.transform = `translateX(-${326 * idx}px)`;
+      setActiveButton(idx);
+    }
+  };
+
+  const onClickPrevBtn = () => {
+    if (ImgUlRef.current && activeButton > 0) {
+      ImgUlRef.current.style.transform = `translateX(-${
+        326 * (activeButton - 1)
+      }px)`;
+      setActiveButton((prev) => prev - 1);
+    }
+  };
+
+  const onClickNextBtn = () => {
+    if (ImgUlRef.current && activeButton < imgArray.length) {
+      ImgUlRef.current.style.transform = `translateX(-${
+        326 * (activeButton + 1)
+      }px)`;
+      setActiveButton((prev) => prev + 1);
+    }
+  };
+  return (
+    <ImgWrapper>
+      {imgArray.length > 1 && (
+        <PrevBtn
+          type='button'
+          disabled={activeButton === 0}
+          onClick={onClickPrevBtn}
+        >
+          <span className='a11y-hidden'>다음</span>
+        </PrevBtn>
+      )}
+      <ImgList ref={ImgUlRef}>
+        {imgArray.map((item, idx) => {
+          return (
+            <ImgItem key={item + idx}>
+              <ProgressiveImg
+                src={item}
+                alt='게시물 이미지'
+                styles={{
+                  objectFit: "cover",
+                  backgroundColor: "#f5f5f5",
+                  verticalAlign: "top",
+                  height: "100%",
+                  width: "100%",
+                  maxWidth: "320px",
+                  maxHeight: "228px",
+                  minHeight: "228px"
+                }}
+              />
+            </ImgItem>
+          );
+        })}
+      </ImgList>
+      {imgArray.length > 1 && (
+        <NextBtn
+          type='button'
+          disabled={activeButton === imgArray.length - 1}
+          onClick={onClickNextBtn}
+        >
+          <span className='a11y-hidden'>이전</span>
+        </NextBtn>
+      )}
+
+      <ImgBtnList>
+        {imgArray.map((image, idx) => {
+          return (
+            <ImgBtnItem key={image + idx}>
+              {imgArray.length > 1 && (
+                <ImgBtn
+                  className={activeButton === idx ? "active" : ""}
+                  onClick={() => onClickSliderBtn(idx)}
+                >
+                  <span className='a11y-hidden'>이미지 슬라이드 버튼</span>
+                </ImgBtn>
+              )}
+            </ImgBtnItem>
+          );
+        })}
+      </ImgBtnList>
+    </ImgWrapper>
+  );
+}
---
+++ src/compoent/units/imgSlider/imgSlider.styles.ts
@@ -0,0 +1,81 @@
+import styled from "styled-components";
+
+export const ImgWrapper = styled.div`
+  position: relative;
+  width: 100%;
+  margin-bottom: 16px;
+  max-height: 228px;
+  overflow: hidden;
+`;
+
+export const ImgList = styled.ul`
+  display: flex;
+  transition: all 0.5s;
+  gap: 6px;
+`;
+
+export const ImgItem = styled.li`
+  width: 100%;
+  height: 100%;
+  border: 0.5px #dbdbdb;
+  overflow: hidden;
+`;
+
+export const ImgBtnList = styled.ul`
+  position: absolute;
+  display: flex;
+  gap: 6px;
+  left: 50%;
+  bottom: 16px;
+  transform: translateX(-50%);
+`;
+
+export const ImgBtnItem = styled.li``;
+
+export const ImgBtn = styled.button`
+  position: relative;
+  width: 8px;
+  height: 8px;
+  border-radius: 50%;
+  background-color: #bdbdbd;
+  transition: all 0.5s;
+  &.active {
+    ::after {
+      position: absolute;
+      top: 50%;
+      left: 50%;
+      transform: translate(-50%, -50%);
+      content: "";
+      width: 5px;
+      height: 5px;
+      border-radius: 50%;
+      background-color: #111;
+    }
+  }
+`;
+
+export const PrevBtn = styled.button`
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  left: 10px;
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  background: url("/assets/icon-prev.svg") no-repeat center left 6px / 15px #fff;
+  opacity: ${(props) => props.disabled && 0.5};
+  z-index: 99;
+`;
+
+export const NextBtn = styled.button`
+  position: absolute;
+  top: 50%;
+  transform: translateY(-50%);
+  right: 10px;
+  width: 30px;
+  height: 30px;
+  border-radius: 50%;
+  background: url("/assets/icon-next.svg") no-repeat center right 6px/ 15px #fff;
+  opacity: ${(props) => props.disabled && 0.5};
+  z-index: 99;
+`;
