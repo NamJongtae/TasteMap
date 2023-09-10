eacf5de77c0bab4b2c6cc69027da6eb10fae3484
MAIN6419 <dark9737@gmail.com>
2023-09-10 21:07:50 +0900

    Feat: 날짜 형식을 변환해주는 setDateFormate 라이브러리 구현

---
+++ src/library/setDateFormat.ts
@@ -0,0 +1,27 @@
+export const setDateFormat = (dateString: Date) => {
+  const now = new Date();
+  const dataTime = new Date(dateString);
+  const diff = Math.floor((now.getTime() - dataTime.getTime()) / 1000);
+
+  const minutes = Math.floor(diff / 60);
+  const hours = Math.floor(diff / (60 * 60));
+  const days = Math.floor(diff / (24 * 60 * 60));
+  const week = Math.floor(diff / (24 * 60 * 60 * 7));
+
+  if (diff < 60) {
+    return "방금 전";
+  } else if (diff < 60 * 60) {
+    return `${minutes}분 전`;
+  } else if (diff < 24 * 60 * 60) {
+    return `${hours}시간 전`;
+  } else if (diff < 24 * 60 * 60 * 7) {
+    return `${days}일 전`;
+  } else if (diff <= 30 * 24 * 60 * 60) {
+    return `${week}주 전`;
+  } else {
+    return `${dataTime.getFullYear()}년 ${(
+      "0" +
+      (dataTime.getMonth() + 1)
+    ).slice(-2)}월 ${("0" + dataTime.getDate()).slice(-2)}일`;
+  }
+};
