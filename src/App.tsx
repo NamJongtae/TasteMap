493002ddb2774a3fbdcdf7aafb784086030a5250
MAIN6419 <dark9737@gmail.com>
2023-09-13 01:30:25 +0900

    Feat: 유저 최신 데이터 유지를 위해 useEffect hook에 로직 추가 및 user.uid을 기준으로 조건부 로그인 유무 구분 onAuthStatechanged 함수를 통해 최신의 유저 데이터를 받아오고 이를 localstorage와 user data state에 적용 기존 user 값을 기준으로 로그인 유무를 판별해서 라우팅 했지만 user data state 초기 값이 빈 객체로 되어 user.uid값을 기준으로 변경

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
 import React, { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { Navigate, Route, Routes } from "react-router-dom";
-import { RootState } from "./store/store";
+import { AppDispatch, RootState } from "./store/store";
 import Login from "./pages/login/Login";
 import DefaultInfo from "./pages/signup/DefaultInfo.container";
 import { detectWebpSupport } from "./library/webpSupport";
 import FindAccount from "./pages/findAccount/FindAccount.container";
 import Home from "./pages/home/Home";
+import { userSlice } from "./slice/userSlice";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
 
 function App() {
   const userData = useSelector((state: RootState) => state.user.data);
+  const dispatch = useDispatch<AppDispatch>();
   // webp 지원유무가 확인 되었을때 컴포넌트를 렌더링 시키위해 사용
   const [webpChecked, setWebpChecked] = useState(false);
 
@@ -20,12 +23,38 @@ function App() {
     } else {
       document.body.classList.add("no-webp");
     }
-    // webp 지원유무가 확인되었다면 true로 설정
+    // webp 지원 유무가 확인되었다면 true로 설정
     setWebpChecked(true);
   };
 
+  // webp 지원 유무 확인
   useEffect(() => {
     checkwebp();
+  }, []);
+
+  // 유저 최신 데이터 갱신
+  useEffect(() => {
+    const auth = getAuth();
+    onAuthStateChanged(auth, async (user) => {
+      // locatlstroage에 유저 정보 저장
+      localStorage.setItem(
+        "user",
+        JSON.stringify({
+          uid: user?.uid,
+          displayName: user?.displayName,
+          email: user?.email,
+          photoURL: user?.photoURL
+        })
+      );
+      dispatch(
+        userSlice.actions.setUser({
+          uid: user?.uid,
+          displayName: user?.displayName,
+          email: user?.email,
+          photoURL: user?.photoURL
+        })
+      );
+    });
   }, []);
 
   return (
@@ -34,19 +63,19 @@ function App() {
         <Routes>
           <Route
             path='/login'
-            element={userData ? <Navigate to='/' /> : <Login />}
+            element={userData.uid ? <Navigate to='/' /> : <Login />}
           />
           <Route
             path='/signup'
-            element={userData ? <Navigate to='/' /> : <DefaultInfo />}
+            element={userData.uid ? <Navigate to='/' /> : <DefaultInfo />}
           />
           <Route
             path='/findAccount'
-            element={userData ? <Navigate to='/' /> : <FindAccount />}
+            element={userData.uid ? <Navigate to='/' /> : <FindAccount />}
           />
           <Route
             path='/'
-            element={!userData ? <Navigate to='/login' /> : <Home />}
+            element={!userData.uid ? <Navigate to='/login' /> : <Home />}
           />
         </Routes>
       )}
