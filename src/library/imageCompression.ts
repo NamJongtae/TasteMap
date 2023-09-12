aa03e26aeef7544766bf958f0a6b200e8c5bf6e8
MAIN6419 <dark9737@gmail.com>
2023-09-13 01:19:27 +0900

    Fix: getCompressionImg type 인자 값 추가 및 상수명 변경 type 값으로 'profile','post' 값을 받아 해당 값에 맞는 이미지 크기 적용 compressedFileBlob 상수명을 compressedBlob으로 변경 preview 상수명을 좀 더 명확한 상수명인 compressedPreview로 변경

--- src/library/imageCompression.ts
+++ src/library/imageCompression.ts
@@ -1,19 +1,24 @@
 import imageCompression from "browser-image-compression"
-
-export const getCompressionImg = async (file:File) => {
+/**
+ * 프로필 이미지를 압축해주는 함수
+ */
+export const getCompressionImg = async (file:File, type: "profile"|"post") => {
   try{
     const options = {
         maxSizeMB: 10, // 이미지 최대 용량
-        maxWidthOrHeight: 256, // 이미지 최대 너비 및 높이
+        maxWidthOrHeight: type==="profile" ? 256 : 320, // 이미지 최대 너비 및 높이
         useWebWorker: true, // webworker 적용 유무
         // webworker : 웹 워커 API가 멀티 스레딩을 지원하게 되어 워커를 이용하면 워커에서 작성된 스크립트는 
         // 메인 스레드에서 분기되어 독립된 스레드로 실행되기 때문에 메모리 자원을 효율적으로 사용할 수 있다.
     }
-
-    const compressedFileBlob = await imageCompression(file, options);
-    const compressedFile = new File([compressedFileBlob], file.name, { type: file.type });
-    const preview = await imageCompression.getDataUrlFromFile(file);
-    return {compressedFile, preview}
+    // 압축한 이미지의 blob 형식으로 가져옴
+    const compressedBlob = await imageCompression(file, options);
+    // blob 형식을 File 형식으로 변환
+    const compressedFile = new File([compressedBlob], file.name, { type: file.type });
+    // 압축한 이미지 미리보기 생성
+    const compressedPreview = await imageCompression.getDataUrlFromFile(file);
+    // 압축한 이미지 파일과 미리보기를 객체형태로 반환
+    return {compressedFile, compressedPreview}
   } catch(error) {
     console.log(error)
   }
