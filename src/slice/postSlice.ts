a1d42e2bf9688640208d9e3c77b188abfaef071f
MAIN6419 <dark9737@gmail.com>
2023-09-13 15:31:14 +0900

    Feat: postSlice dummy 데이터 제거 및 initalState 수정/추가, thuck 비동기 처리 함수 추가, reducer 추가 initalState data 값을 dummy 데이터 에서 []로 변경, 검색 결과인 searchMapData, 검색 결과 중 선택한 데이터인 selectedMapData 추가, 로딩을 위해 isLoading 추가 맛집 검색 처리 thuckFetchSearchMap, 게시물 업로드 thunkFetchUploadPost 비동기 처리 함수 추가 맛집 검색 결과 데이터를 업데이트 하는 setSelectedMapData, 맛집 검색 데이터를 초기화 하는 resetSearchMapData, 맛집 검색 결과 선택 데이터 초기화 하는 resetSelectedMapData reducer 추가

--- src/slice/postSlice.ts
+++ src/slice/postSlice.ts
@@ -1,76 +1,101 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { IPostData } from "../api/firebase/firebaseAPIType";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import { IPostData, IPostUploadData, ISearchMapData } from "../api/apiType";
+import { fetchSearchData } from "../api/naverSearchAPI/naverSearchAPI";
+import { fetchPostImg, fetchUploadPost } from "../api/firebase/uploadAPI";
 
-const dummy: IPostData[] = [
-  {
-    uid: "aaa",
-    displayName: "미식가",
-    content: "여기 맛집이네요",
-    img: ["/assets/test.jpg"],
-    createdAt: new Date("2023-07-07"),
-    likeCount: 3,
-    commentCount: 3,
-    id: "0",
-    coords: { x: 35.6031, y: 127.5391 }
-  },
-  {
-    uid: "bbb",
-    displayName: "먹깨비",
-    content: "여기로 오세요",
-    img: ["/assets/asd.jpg"],
-    createdAt: new Date("2023-09-08"),
-    likeCount: 2,
-    commentCount: 1,
-    id: "1",
-    coords: { x: 35.6031, y: 127.5391 }
-  },
-  {
-    uid: "ccc",
-    displayName: "맛집",
-    content: "여기 별로네요",
-    img: ["/assets/test.jpg"],
-    createdAt: new Date("2023-09-09"),
-    likeCount: 5,
-    commentCount: 3,
-    id: "2",
-    coords: { x: 35.6031, y: 127.5391 }
-  },
-  {
-    uid: "ddd",
-    displayName: "test",
-    content: "와우",
-    img: ["/assets/test.jpg"],
-    createdAt: new Date(),
-    likeCount: 5,
-    commentCount: 3,
-    id: "3",
-    coords: { x: 35.6031, y: 127.5391 }
-  },
-  {
-    uid: "eee",
-    displayName: "미식가",
-    content: "여기 맛집이네요",
-    img: [],
-    createdAt: new Date("2023-07-07"),
-    likeCount: 3,
-    commentCount: 3,
-    id: "4",
-    coords: { x: 35.6031, y: 127.5391 }
+interface IknownError {
+  message: string;
+}
+
+// 맛집 검색
+export const thuckFetchSearchMap = createAsyncThunk(
+  "postSlice/thuckFetchSearchMap",
+  async (keyword: string, thunkAPI) => {
+    try {
+      const res = await fetchSearchData(keyword);
+      return res;
+    } catch (error: any) {
+      return thunkAPI.rejectWithValue(error);
+    }
   }
-];
+);
+
+// 게시물 업로드
+export const thunkFetchUploadPost = createAsyncThunk<
+  void,
+  IPostUploadData,
+  { rejectValue: IknownError }
+>("postSlice/thunkFetchUploadPost", async (postData, thunkAPI) => {
+  try {
+    // img 프로퍼티는 이미지 firestore에 이미지를 저장하고
+    // 저장한 이미지 파일의 url과 filename를 얻기위해 사용
+    if (postData.img) {
+      const res = await fetchPostImg(postData.img);
+      postData.imgURL = res.url;
+      postData.imgName = res.filename;
+    }
+    // 이미지 저장, 이미지 저장 데이터 적용 후
+    // 이미지 파일 프로퍼티 삭제
+    // 데이터 업로드시 파일 프로퍼티는 업로드할 필요가 없음
+    delete postData.img;
+    await fetchUploadPost(postData);
+  } catch (error: any) {
+    return thunkAPI.rejectWithValue(error);
+  }
+});
+
 export const postSlice = createSlice({
   name: "postSlice",
   initialState: {
-    data: dummy,
+    postData: [] as IPostData[], // 게시물 데이터
+    searchMapData: [] as ISearchMapData[], // 검색 데이터
+    seletedMapData: [] as ISearchMapData[], // 선택한 검색 데이터
     error: "",
-    isLoading: ""
+    isLoading: false
   },
   reducers: {
+    setSelectedMapData: (state, action) => {
+      state.seletedMapData = [action.payload];
+    },
+    resetSelectedMapData: (state) => {
+      state.seletedMapData = [] as ISearchMapData[];
+    },
+    resetSearchMapData: (state) => {
+      state.searchMapData = [];
+    },
     remove: (state, action) => {
-      const newData = [...state.data].filter(
+      const newData = [...state.postData].filter(
         (item) => item.id !== action.payload
       );
-      state.data = newData;
+      state.postData = newData;
+    },
+    setIsLoading: (state, action) => {
+      state.isLoading = action.payload;
     }
+  },
+  extraReducers: (builder) => {
+    // 맛집 지도 검색
+    builder.addCase(thuckFetchSearchMap.fulfilled, (state, action) => {
+      state.searchMapData = action.payload;
+    });
+    builder.addCase(thuckFetchSearchMap.rejected, (state, action) => {
+      if (!action.payload) return;
+      state.error = action.payload.toString();
+    });
+
+    // 게시물 업로드
+    builder.addCase(thunkFetchUploadPost.pending, (state) => {
+      document.body.style.overflow = "hidden";
+      state.isLoading = true;
+    });
+    builder.addCase(thunkFetchUploadPost.fulfilled, (state) => {
+      document.body.style.overflow = "auto";
+      state.isLoading = false;
+    });
+    builder.addCase(thunkFetchUploadPost.rejected, (state, action) => {
+      if (!action.payload) return;
+      state.isLoading = false;
+      state.error = action.payload.toString();
+    });
   }
 });
