9887f4f62bcd16a25aeba09d1e1532bf69bd9b20
MAIN6419 <dark9737@gmail.com>
2023-09-16 00:24:53 +0900

    Feat: PostEdit 컴포넌트 구현 기존 PostUpload 컴포넌트에 props에 isEdit true값을 넘겨주어 PostUpload 컴포넌트를 재사용하여 구현

---
+++ src/pages/post/postEdit/PostEdit.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import PostUpload from '../../postUpload/PostUpload.container'
+
+export default function PostEdit() {
+  return (
+    <PostUpload isEdit={true}/>
+  )
+}
