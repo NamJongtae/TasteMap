1fb9003e5e3b37e9afc8fd7964475391e25fd344
MAIN6419 <dark9737@gmail.com>
2023-09-18 20:41:42 +0900

    Fix: Loading 컴포넌트 로딩 gif 이미지가 로딩이 완료된 후 Loading 텍스트가 표시되도록 수정 gif 이미지 로딩이 안된 상태에서 Loading 컴포넌트 출력시 Loading 중 텍스트가 먼저 표시되고 이후 gif 이미지가 나와 UX적으로 좋지 않기 때문에 gif 이미지가 로딩이 완료된 후 Loading 텍스트가 표시되도록 수정

--- src/compoent/commons/loading/Loading.tsx
+++ src/compoent/commons/loading/Loading.tsx
@@ -1,12 +1,28 @@
-import React from 'react'
-import { LoadingImg, LoadingText, Title, Wrapper } from './loading.styles'
+import React, { useEffect, useState } from "react";
+import { LoadingImg, LoadingText, Title, Wrapper } from "./loading.styles";
 
 export default function Loading() {
+  const [imageLoaded, setImageLoaded] = useState(false);
+
+  // 로딩 gif 이미지 로딩 상태를 확인하고 이미지가 로딩 완료되면 상태를 변경합니다.
+  useEffect(() => {
+    const img = new Image();
+    img.src = "/assets/spinner.gif";
+    img.onload = () => {
+      setImageLoaded(true);
+    };
+  }, []);
+ 
   return (
     <Wrapper>
       <Title className='a11y-hidden'>로딩중</Title>
-      <LoadingImg src='/assets/spinner.gif' alt='Loading'/>
-      <LoadingText>Loading...</LoadingText>
+      {/* 로딩 이미지 gif가 로딩 완료된 후 Loading 이미지와 텍스트 출력 */}
+      {imageLoaded && (
+        <>
+          <LoadingImg src='/assets/spinner.gif' alt='Loading' />
+          <LoadingText>Loading...</LoadingText>
+        </>
+      )}
     </Wrapper>
-  )
+  );
 }
