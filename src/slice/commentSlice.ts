06fe501ef9a9e16fd54e323660e7f00a1f249fea
MAIN6419 <dark9737@gmail.com>
2023-09-21 21:51:47 +0900

    Feat: commentSlice 댓글 신고 및 댓글 첫 페이지 데이터, 댓글 페이징 함수 redux-thunk를 통해 비동기 처리 함수 구현, initalState 추가 initalState 댓글의 페이지를 나타내는 page, 페이지당 데이터의 개수인 pagePerData, 다음 데이터가 있는지 나타내는 hasMore state 추가

--- src/slice/commentSlice.ts
+++ src/slice/commentSlice.ts
@@ -2,24 +2,64 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import { ICommentData, IKnownError } from "../api/apiType";
 import {
   fetchAddComment,
-  fetchCommentList,
   fetchEditComment,
+  fetchFirstPageCommentData,
+  fetchPagingCommentData,
   fetchRemoveComment,
+  fetchReportComment
 } from "../api/firebase/commentAPI";
 import { sweetToast } from "../library/sweetAlert/sweetAlert";
+import {
+  DocumentData,
+  QueryDocumentSnapshot,
+  QuerySnapshot
+} from "firebase/firestore";
 
-export const thunkFetchCommentList = createAsyncThunk<
-  ICommentData[] | undefined,
-  string,
+// 댓글 첫 페이지 조회
+export const thunkFetchFirstPageCommentData = createAsyncThunk<
+  | {
+      commentDoc: QuerySnapshot<DocumentData, DocumentData>;
+      data: ICommentData[];
+    }
+  | undefined,
+  { postId: string; pagePerData: number },
   { rejectValue: IKnownError }
->("commentSlice/thunkFetchCommentList", async (postId, thunkAPI) => {
-  try {
-    const res = await fetchCommentList(postId);
-    return res;
-  } catch (error: any) {
-    thunkAPI.rejectWithValue(error);
+>(
+  "commentSlice/thunkFetchFirstPageCommentData",
+  async ({ postId, pagePerData }, thunkAPI) => {
+    try {
+      const res = await fetchFirstPageCommentData(postId, pagePerData);
+      return res;
+    } catch (error: any) {
+      thunkAPI.rejectWithValue(error);
+    }
   }
-});
+);
+
+// 댓글 페이징 
+export const thunkFetchPagingCommentData = createAsyncThunk<
+  | {
+      commentDoc: QuerySnapshot<DocumentData, DocumentData>;
+      data: ICommentData[];
+    }
+  | undefined,
+  {
+    page: QueryDocumentSnapshot<DocumentData, DocumentData>;
+    postId: string;
+    pagePerData: number;
+  },
+  { rejectValue: IKnownError }
+>(
+  "commentSlice/thunkFetchPagingCommentData",
+  async ({ page, postId, pagePerData }, thunkAPI) => {
+    try {
+      const res = await fetchPagingCommentData(page, postId, pagePerData);
+      return res;
+    } catch (error: any) {
+      thunkAPI.rejectWithValue(error);
+    }
+  }
+);
 
 export const thunkFetchAddComment = createAsyncThunk<
   ICommentData | undefined,
@@ -60,10 +100,30 @@ export const thunkFetchRemoveComment = createAsyncThunk<
   }
 });
 
+export const thunkFetchReportComment = createAsyncThunk<
+  | (Pick<ICommentData, "commentId" | "reportCount"> & { postId: string })
+  | undefined,
+  Pick<ICommentData, "commentId" | "reportCount"> & { postId: string },
+  { rejectValue: IKnownError }
+>(
+  "commentSlice/thunkFetchReportComment",
+  async (reportCommentData, thunkAPI) => {
+    try {
+      await fetchReportComment(reportCommentData);
+      return reportCommentData;
+    } catch (error: any) {
+      thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+
 export const commentSlice = createSlice({
   name: "commentSlice",
   initialState: {
     commentListData: [] as ICommentData[],
+    page: {} as QueryDocumentSnapshot<DocumentData>,
+    pagePerData: 10,
+    hasMore: false,
     postId: "",
     isOpenCommentModal: false,
     isLoading: false,
@@ -78,21 +138,49 @@ export const commentSlice = createSlice({
     },
     setIsOpenCommentModal: (state, action) => {
       state.isOpenCommentModal = action.payload;
-    },
+    }
   },
   extraReducers: (builder) => {
-    // 댓글 조회
-    builder.addCase(thunkFetchCommentList.pending, (state) => {
+    // 댓글 첫 페이지 조회
+    builder.addCase(thunkFetchFirstPageCommentData.pending, (state) => {
       state.isLoading = true;
     });
-    builder.addCase(thunkFetchCommentList.fulfilled, (state, action) => {
-      if (action.payload) state.commentListData = action.payload;
-      state.isLoading = false;
-    });
-    builder.addCase(thunkFetchCommentList.rejected, (state, action) => {
-      if (action.payload) state.error = action.payload?.message;
-      console.error(state.error);
-      state.isLoading = false;
+    builder.addCase(
+      thunkFetchFirstPageCommentData.fulfilled,
+      (state, action) => {
+        if (action.payload) {
+          state.commentListData = action.payload.data as ICommentData[];
+          state.hasMore =
+            (action.payload?.data as ICommentData[]).length %
+              state.pagePerData ===
+            0;
+          state.page =
+            action.payload.commentDoc.docs[
+              action.payload.commentDoc.docs.length - 1
+            ];
+        }
+        state.isLoading = false;
+      }
+    );
+    builder.addCase(
+      thunkFetchFirstPageCommentData.rejected,
+      (state, action) => {
+        if (action.payload) state.error = action.payload?.message;
+        console.error(state.error);
+        state.isLoading = false;
+      }
+    );
+
+    // 댓글 페이징
+    builder.addCase(thunkFetchPagingCommentData.fulfilled, (state, action) => {
+      if (!action.payload) return;
+      state.commentListData = [
+        ...state.commentListData,
+        ...(action.payload?.data as ICommentData[])
+      ];
+      state.page =
+        action.payload.commentDoc.docs[action.payload.commentDoc.docs.length - 1];
+      state.hasMore = action.payload.data.length % state.pagePerData === 0;
     });
 
     // 댓글 추가
@@ -140,5 +228,27 @@ export const commentSlice = createSlice({
       }
     });
 
+    // 댓글 신고
+    builder.addCase(thunkFetchReportComment.fulfilled, (state, action) => {
+      if (action.payload && action.payload?.reportCount >= 4) {
+        const newData = [...state.commentListData].filter(
+          (item) => item.commentId !== action.payload?.commentId
+        );
+        state.commentListData = newData;
+        sweetToast(
+          "신고가 누적되어 댓글이 블라인드 처리되었습니다.",
+          "info",
+          1500
+        );
+      } else {
+        sweetToast("신고가 완료되었습니다.", "success");
+      }
+    });
+    builder.addCase(thunkFetchReportComment.rejected, (state, action) => {
+      if (action.payload) {
+        state.error = action.payload.message;
+        console.error(state.error);
+      }
+    });
   }
 });
