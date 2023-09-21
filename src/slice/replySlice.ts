4b2676445d367765989f3b2ed98db9c72df6e660
MAIN6419 <dark9737@gmail.com>
2023-09-21 21:54:05 +0900

    Feat: replySlice 답글 신고 및 답글 첫 페이지 데이터, 답글 페이징 함수 redux-thunk를 통해 비동기 처리 적용, initalState 추가 initalState 답글의 페이지를 나타내는 page, 페이지당 데이터의 개수인 pagePerData, 다음 데이터가 있는지 나타내는 hasMore state 추가

--- src/slice/replySlice.ts
+++ src/slice/replySlice.ts
@@ -3,10 +3,60 @@ import { IKnownError, IReplyData } from "../api/apiType";
 import {
   fetchAddReply,
   fetchEditReply,
+  fetchFirstPageReplyData,
+  fetchPagingReplyData,
   fetchRemoveReply,
-  fetchReplyListData
+  fetchReplyListData,
+  fetchReportReply
 } from "../api/firebase/replyAPI";
 import { sweetToast } from "../library/sweetAlert/sweetAlert";
+import { DocumentData, QueryDocumentSnapshot, QuerySnapshot } from 'firebase/firestore';
+
+// 답글 첫 페이지 조회
+export const thunkFetchFirstPageReplyData = createAsyncThunk<
+  | {
+      replyDoc: QuerySnapshot<DocumentData, DocumentData>;
+      data: IReplyData[];
+    }
+  | undefined,
+  { parentCommentId: string; pagePerData: number },
+  { rejectValue: IKnownError }
+>(
+  "replySlice/thunkFetchFirstPageReplyData",
+  async ({ parentCommentId, pagePerData }, thunkAPI) => {
+    try {
+      const res = await fetchFirstPageReplyData(parentCommentId, pagePerData);
+      return res;
+    } catch (error: any) {
+      thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+
+// 답글 페이징 
+export const thunkFetchPagingReplyData = createAsyncThunk<
+  | {
+      replyDoc: QuerySnapshot<DocumentData, DocumentData>;
+      data: IReplyData[];
+    }
+  | undefined,
+  {
+    page: QueryDocumentSnapshot<DocumentData, DocumentData>;
+    parentCommentId: string;
+    pagePerData: number;
+  },
+  { rejectValue: IKnownError }
+>(
+  "replySlice/thunkFetchPagingReplyData",
+  async ({ page, parentCommentId, pagePerData }, thunkAPI) => {
+    try {
+      const res = await fetchPagingReplyData(page, parentCommentId, pagePerData);
+      return res;
+    } catch (error: any) {
+      thunkAPI.rejectWithValue(error);
+    }
+  }
+);
 
 /**
  * 답글 조회
@@ -15,7 +65,7 @@ export const thunkFetchReplyListData = createAsyncThunk<
   IReplyData[] | undefined,
   string,
   { rejectValue: IKnownError }
->("commentSlice/thunkFetchReplyListData", async (parentCommentId, thunkAPI) => {
+>("replySlice/thunkFetchReplyListData", async (parentCommentId, thunkAPI) => {
   try {
     const res = await fetchReplyListData(parentCommentId);
     return res;
@@ -31,7 +81,7 @@ export const thunkFetchAddReply = createAsyncThunk<
   IReplyData | undefined,
   IReplyData,
   { rejectValue: IKnownError }
->("commentSlice/thunkFetchAddReply", async (replyData, thunkAPI) => {
+>("replySlice/thunkFetchAddReply", async (replyData, thunkAPI) => {
   try {
     await fetchAddReply(replyData);
     return replyData;
@@ -47,7 +97,7 @@ export const thunkFetchEditReply = createAsyncThunk<
   Pick<IReplyData, "parentCommentId" | "replyId" | "content"> | undefined,
   Pick<IReplyData, "parentCommentId" | "replyId" | "content">,
   { rejectValue: IKnownError }
->("commentSlice/thunkFetchEditReply", async (replyEditData, thunkAPI) => {
+>("replySlice/thunkFetchEditReply", async (replyEditData, thunkAPI) => {
   try {
     await fetchEditReply(replyEditData);
     return replyEditData;
@@ -63,7 +113,7 @@ export const thunkFetchRemoveReply = createAsyncThunk<
   Pick<IReplyData, "parentCommentId" | "replyId"> | undefined,
   Pick<IReplyData, "parentCommentId" | "replyId">,
   { rejectValue: IKnownError }
->("commentSlice/thunkFetchRemoveReply", async (replyRemoveData, thunkAPI) => {
+>("replySlice/thunkFetchRemoveReply", async (replyRemoveData, thunkAPI) => {
   try {
     await fetchRemoveReply(replyRemoveData);
     return replyRemoveData;
@@ -72,10 +122,29 @@ export const thunkFetchRemoveReply = createAsyncThunk<
   }
 });
 
+/**
+ * 답글 신고
+ */
+export const thunkFetchReportReply = createAsyncThunk<
+  Pick<IReplyData, "replyId" | "parentCommentId" | "reportCount"> | undefined,
+  Pick<IReplyData, "replyId" | "parentCommentId" | "reportCount">,
+  { rejectValue: IKnownError }
+>("replySlice/thunkFetchReportReply", async (reportReplyData, thunkAPI) => {
+  try {
+    await fetchReportReply(reportReplyData);
+    return reportReplyData;
+  } catch (error: any) {
+    thunkAPI.rejectWithValue(error);
+  }
+});
+
 export const replySlice = createSlice({
-  name: "commentSlice",
+  name: "replySlice",
   initialState: {
     replyListData: [] as IReplyData[],
+    page: {} as QueryDocumentSnapshot<DocumentData>,
+    pagePerData: 10,
+    hasMore: false,
     parentCommentId: "",
     isOpenReplyModal: false,
     isLoading: false,
@@ -90,18 +159,46 @@ export const replySlice = createSlice({
     }
   },
   extraReducers: (builder) => {
-    // 답글 조회
-    builder.addCase(thunkFetchReplyListData.pending, (state) => {
+    // 답글 첫 페이지 조회
+    builder.addCase(thunkFetchFirstPageReplyData.pending, (state) => {
       state.isLoading = true;
     });
-    builder.addCase(thunkFetchReplyListData.fulfilled, (state, action) => {
-      if (action.payload) state.replyListData = action.payload;
-      state.isLoading = false;
-    });
-    builder.addCase(thunkFetchReplyListData.rejected, (state, action) => {
-      if (action.payload) state.error = action.payload?.message;
-      console.error(state.error);
-      state.isLoading = false;
+    builder.addCase(
+      thunkFetchFirstPageReplyData.fulfilled,
+      (state, action) => {
+        if (action.payload) {
+          state.replyListData = action.payload.data as IReplyData[];
+          state.hasMore =
+            (action.payload?.data as IReplyData[]).length %
+              state.pagePerData ===
+            0;
+          state.page =
+            action.payload.replyDoc.docs[
+              action.payload.replyDoc.docs.length - 1
+            ];
+        }
+        state.isLoading = false;
+      }
+    );
+    builder.addCase(
+      thunkFetchFirstPageReplyData.rejected,
+      (state, action) => {
+        if (action.payload) state.error = action.payload?.message;
+        console.error(state.error);
+        state.isLoading = false;
+      }
+    );
+
+    // 답글 페이징
+    builder.addCase(thunkFetchPagingReplyData.fulfilled, (state, action) => {
+      if (!action.payload) return;
+      state.replyListData = [
+        ...state.replyListData,
+        ...(action.payload?.data as IReplyData[])
+      ];
+      state.page =
+        action.payload.replyDoc.docs[action.payload.replyDoc.docs.length - 1];
+      state.hasMore = action.payload.data.length % state.pagePerData === 0;
     });
 
     // 답글 추가
@@ -150,6 +247,29 @@ export const replySlice = createSlice({
       }
     });
     builder.addCase(thunkFetchRemoveReply.rejected, (state, action) => {
+      if (action.payload) {
+        state.error = action.payload.message;
+        console.error(state.error);
+      }
+    });
+
+    // 답글 신고
+    builder.addCase(thunkFetchReportReply.fulfilled, (state, action) => {
+      if (action.payload && action.payload?.reportCount >= 4) {
+        const newData = [...state.replyListData].filter(
+          (item) => item.replyId !== action.payload?.replyId
+        );
+        state.replyListData = newData;
+        sweetToast(
+          "신고가 누적되어 답글이 블라인드 처리되었습니다.",
+          "info",
+          1500
+        );
+      } else {
+        sweetToast("신고가 완료되었습니다.", "success");
+      }
+    });
+    builder.addCase(thunkFetchReportReply.rejected, (state, action) => {
       if (action.payload) {
         state.error = action.payload.message;
         console.error(state.error);
