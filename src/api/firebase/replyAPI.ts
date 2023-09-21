a8a83ca60d0c2d928556a43ee4940cc2665aeb0e
MAIN6419 <dark9737@gmail.com>
2023-09-21 21:51:59 +0900

    Feat: replyAPI 댓글 신고 API 함수 및 무한 스크롤 구현을 위한 API 함수 구현, try catch문 예외 처리 적용 답글을 신고하는 fetchReportReply API 함수 구현 reportCount가 5가 되면 댓글이 블라이드 처리 블라인드 처리 후 댓글의 답글 카운터를 1 감소시킴 무한 스크롤 구현을 위해 첫 페이지를 불러오는 fetchFirstPageReplyData API 함수 및 이후 댓글 페이징 처리를 위한 fetchPagingReplyData API 함수 구현 파이어베이스의 startAfter() query를 이용하여 페이징 처리 이를 위해 docs를 page로 설정 에러 예외처리를 위한 try catch문 적용

--- src/api/firebase/replyAPI.ts
+++ src/api/firebase/replyAPI.ts
@@ -7,63 +7,170 @@ import {
   setDoc,
   updateDoc,
   increment,
-  deleteDoc
+  deleteDoc,
+  arrayUnion,
+  limit,
+  QueryDocumentSnapshot,
+  DocumentData,
+  startAfter
 } from "firebase/firestore";
 import { IReplyData } from "../apiType";
 import { db } from "./setting";
+import { getAuth } from "firebase/auth";
+
+const auth = getAuth();
 
 /**
- * 답글 조회
+ * 답글 첫 페이지 조회
  */
+export const fetchFirstPageReplyData = async (
+  parentCommentId: string, pagePerData:number
+) => {
+  try {
+    const commentDoc = doc(db, `comments/${parentCommentId}`);
+    const replyRef = collection(commentDoc, "replies");
+    const q = query(
+      replyRef,
+      orderBy("createdAt", "desc"),
+      limit(pagePerData),
+    );
+    const replyDoc = await getDocs(q);
+    const data = replyDoc.docs.map((el) => el.data());
+
+    const userRef = collection(db, "user");
+    const userDocs = await getDocs(userRef);
+    const userList = userDocs.docs.map((el) => el.data());
+
+    // 유저 db를 담을 Map 생성
+    const userMap = new Map();
+    // 사용자 데이터 목록을 반복하면서 Map에 사용자 UID를 키로 하여 데이터를 저장
+    for (const userData of userList) {
+      userMap.set(userData.uid, userData);
+    }
+    // 게시물 데이터 목록을 반복하면서 사용자 데이터를 연결
+    for (let i = 0; i < data.length; i++) {
+      const userData = userMap.get(data[i].uid);
+      // 기존 data에 displayName과 photoURL을 추가
+      if (userData) {
+        data[i].displayName = userData.displayName;
+        data[i].photoURL = userData.photoURL;
+      }
+    }
+
+    return {replyDoc, data: data as IReplyData[]};
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+/**
+ * 답글 페이징
+ */
+export const fetchPagingReplyData = async (
+  page: QueryDocumentSnapshot<DocumentData, DocumentData>,
+  parentCommentId: string, pagePerData:number
+) => {
+  try {
+    const commentDoc = doc(db, `comments/${parentCommentId}`);
+    const replyRef = collection(commentDoc, `replies`);
+    const q = query(
+      replyRef,
+      orderBy("createdAt", "desc"),
+      startAfter(page),
+      limit(pagePerData),
+    );
+    const replyDoc = await getDocs(q);
+    const data = replyDoc.docs.map((el) => el.data());
+
+    const userRef = collection(db, "user");
+    const userDocs = await getDocs(userRef);
+    const userList = userDocs.docs.map((el) => el.data());
+
+    // 유저 db를 담을 Map 생성
+    const userMap = new Map();
+    // 사용자 데이터 목록을 반복하면서 Map에 사용자 UID를 키로 하여 데이터를 저장
+    for (const userData of userList) {
+      userMap.set(userData.uid, userData);
+    }
+    // 게시물 데이터 목록을 반복하면서 사용자 데이터를 연결
+    for (let i = 0; i < data.length; i++) {
+      const userData = userMap.get(data[i].uid);
+      // 기존 data에 displayName과 photoURL을 추가
+      if (userData) {
+        data[i].displayName = userData.displayName;
+        data[i].photoURL = userData.photoURL;
+      }
+    }
+
+    return {replyDoc, data: data as IReplyData[]};
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
 export const fetchReplyListData = async (
   parentCommentId: string
 ): Promise<IReplyData[]> => {
-  const commentDoc = doc(db, `comments/${parentCommentId}`);
-  const replyRef = collection(commentDoc, `replies`);
-  const q = query(replyRef, orderBy("createdAt", "desc"));
-  const res = await getDocs(q);
-  const data = res.docs.map((el) => el.data());
+  try {
+    const commentDoc = doc(db, `comments/${parentCommentId}`);
+    const replyRef = collection(commentDoc, `replies`);
+    const q = query(replyRef, orderBy("createdAt", "desc"));
+    const res = await getDocs(q);
+    const data = res.docs.map((el) => el.data());
 
-  const userRef = collection(db, "user");
-  const userDocs = await getDocs(userRef);
-  const userList = userDocs.docs.map((el) => el.data());
+    const userRef = collection(db, "user");
+    const userDocs = await getDocs(userRef);
+    const userList = userDocs.docs.map((el) => el.data());
 
-  // 유저 db를 담을 Map 생성
-  const userMap = new Map();
-  // 사용자 데이터 목록을 반복하면서 Map에 사용자 UID를 키로 하여 데이터를 저장
-  for (const userData of userList) {
-    userMap.set(userData.uid, userData);
+    // 유저 db를 담을 Map 생성
+    const userMap = new Map();
+    // 사용자 데이터 목록을 반복하면서 Map에 사용자 UID를 키로 하여 데이터를 저장
+    for (const userData of userList) {
+      userMap.set(userData.uid, userData);
+    }
+
+    // 게시물 데이터 목록을 반복하면서 사용자 데이터를 연결
+    for (let i = 0; i < data.length; i++) {
+      const userData = userMap.get(data[i].uid);
+      // 기존 data에 displayName과 photoURL을 추가
+      if (userData) {
+        data[i].displayName = userData.displayName;
+        data[i].photoURL = userData.photoURL;
+      }
+    }
+
+    return data as IReplyData[];
+  } catch (error) {
+    console.error(error);
+    throw error;
   }
-
-  // 게시물 데이터 목록을 반복하면서 사용자 데이터를 연결
-  for (let i = 0; i < data.length; i++) {
-    const userData = userMap.get(data[i].uid);
-    // 기존 data에 displayName과 photoURL을 추가
-    data[i].displayName = userData.displayName;
-    data[i].photoURL = userData.photoURL;
-  }
-
-  return data as IReplyData[];
 };
 
 /**
  * 답글 추가
  */
 export const fetchAddReply = async (replyCommentData: IReplyData) => {
-  const commentDoc = doc(db, `comments/${replyCommentData.parentCommentId}`);
-  const replyRef = collection(commentDoc, "replies");
-  // 댓글 데이터 subCollection replies에 답글 Doc 추가
-  const addReplyCommentProimse = setDoc(
-    doc(replyRef, replyCommentData.replyId),
-    { ...replyCommentData }
-  );
+  try {
+    const commentDoc = doc(db, `comments/${replyCommentData.parentCommentId}`);
+    const replyRef = collection(commentDoc, "replies");
+    // 댓글 데이터 subCollection replies에 답글 Doc 추가
+    const addReplyCommentProimse = setDoc(
+      doc(replyRef, replyCommentData.replyId),
+      { ...replyCommentData }
+    );
 
-  // 댓글 데이터 답글 수 늘리기
-  const addReplyCountPromise = updateDoc(commentDoc, {
-    replyCount: increment(1)
-  });
+    // 댓글 데이터 답글 수 늘리기
+    const addReplyCountPromise = updateDoc(commentDoc, {
+      replyCount: increment(1)
+    });
 
-  await Promise.all([addReplyCommentProimse, addReplyCountPromise]);
+    await Promise.all([addReplyCommentProimse, addReplyCountPromise]);
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 };
 
 /**
@@ -72,12 +179,17 @@ export const fetchAddReply = async (replyCommentData: IReplyData) => {
 export const fetchEditReply = async (
   replyEditData: Pick<IReplyData, "parentCommentId" | "replyId" | "content">
 ) => {
-  const commentDoc = doc(db, `comments/${replyEditData.parentCommentId}`);
-  const replyDoc = doc(commentDoc, `replies/${replyEditData.replyId}`);
-  // 답글 doc의 content 수정
-  await updateDoc(replyDoc, {
-    content: replyEditData.content
-  });
+  try {
+    const commentDoc = doc(db, `comments/${replyEditData.parentCommentId}`);
+    const replyDoc = doc(commentDoc, `replies/${replyEditData.replyId}`);
+    // 답글 doc의 content 수정
+    await updateDoc(replyDoc, {
+      content: replyEditData.content
+    });
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 };
 
 /**
@@ -86,15 +198,55 @@ export const fetchEditReply = async (
 export const fetchRemoveReply = async (
   replyEditData: Pick<IReplyData, "parentCommentId" | "replyId">
 ) => {
-  const commentDoc = doc(db, `comments/${replyEditData.parentCommentId}`);
-  const replyDoc = doc(commentDoc, `replies/${replyEditData.replyId}`);
-  // 답글 doc 삭제
-  const removeReplyPromise =  deleteDoc(replyDoc);
+  try {
+    const commentDoc = doc(db, `comments/${replyEditData.parentCommentId}`);
+    const replyDoc = doc(commentDoc, `replies/${replyEditData.replyId}`);
+    // 답글 doc 삭제
+    const removeReplyPromise = deleteDoc(replyDoc);
 
-  // 댓글 doc의 replyCount 줄이기
-  const decreaseReplyCountPromise = updateDoc(commentDoc, {
-    replyCount: increment(-1)
-  });
+    // 댓글 doc의 replyCount 줄이기
+    const decreaseReplyCountPromise = updateDoc(commentDoc, {
+      replyCount: increment(-1)
+    });
 
-  await Promise.all([removeReplyPromise, decreaseReplyCountPromise]);
+    await Promise.all([removeReplyPromise, decreaseReplyCountPromise]);
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
+
+/**
+ * 답글 신고
+ */
+export const fetchReportReply = async (
+  replyReportData: Pick<IReplyData, "replyId" | "parentCommentId" | "reportCount">
+) => {
+  try {
+    const commentDoc = doc(db, `comments/${replyReportData.parentCommentId}`);
+    const replyDoc = doc(commentDoc, `replies/${replyReportData.replyId}`);
+    const addReportCountPromise = await updateDoc(replyDoc, {
+      reportCount: increment(1),
+      isBlock: replyReportData.reportCount >= 4 ? true : false
+    });
+
+    const decreaseReplyCountPromise = []
+    if (replyReportData.reportCount >= 4) {
+      const commentDoc = doc(db, `comments/${replyReportData.parentCommentId}`);
+      decreaseReplyCountPromise.push(updateDoc(commentDoc, {
+        replyCount: increment(-1)
+      }));
+    }
+
+    if (!auth.currentUser) return;
+    const userDoc = doc(db, `user/${auth.currentUser.uid}`);
+    const addReportListPromise = updateDoc(userDoc, {
+      reportReplyList: arrayUnion(replyReportData.replyId)
+    });
+
+    await Promise.all([addReportCountPromise, addReportListPromise]);
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 };
