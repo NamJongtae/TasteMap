14a38f951e43d4701553f60aeb6e65161e81deef
MAIN6419 <dark9737@gmail.com>
2023-09-21 21:40:39 +0900

    Feat: commentAPI 댓글 신고 API 함수 및 무한 스크롤 구현을 위한 API 함수 구현, try catch문 예외 처리 적용 댓글을 신고하는 fetchReportCommet API 함수 구현 reportCount가 5가 되면 댓글이 블라이드 처리 블라인드 처리 후 게시물의 댓글 카운터를 1 감소시킴 무한 스크롤 구현을 위해 첫 페이지를 불러오는 fetchFirstPageCommentData API 함수 및 이후 댓글 페이징 처리를 위한 fetchPagingCommentData API 함수 구현 파이어베이스의 startAfter() query를 이용하여 페이징 처리 이를 위해 docs를 page로 설정 에러 예외처리를 위한 try catch문 적용

--- src/api/firebase/commentAPI.ts
+++ src/api/firebase/commentAPI.ts
@@ -1,74 +1,139 @@
 import {
+  DocumentData,
+  QueryDocumentSnapshot,
+  arrayUnion,
   collection,
   deleteDoc,
   doc,
   getDocs,
   increment,
+  limit,
   orderBy,
   query,
   setDoc,
+  startAfter,
   updateDoc,
   where
 } from "firebase/firestore";
 import { db } from "./setting";
 import { ICommentData } from "../apiType";
+import { getAuth } from "firebase/auth";
+
+const auth = getAuth();
 
 /**
- * 댓글 조회
+ * 댓글 첫 페이지 조회
  */
-export const fetchCommentList = async (
-  postId: string
-): Promise<ICommentData[]> => {
+export const fetchFirstPageCommentData = async (
+  postId: string, pagePerData:number
+) => {
+  try {
+    const commentsRef = collection(db, "comments");
+    const q = query(
+      commentsRef,
+      orderBy("createdAt", "desc"),
+      where("postId", "==", postId),
+      limit(pagePerData),
+    );
+    const commentDoc = await getDocs(q);
+    const data = commentDoc.docs.map((el) => el.data());
 
-  const commentsRef = collection(db, "comments");
-  const q = query(
-    commentsRef,
-    orderBy("createdAt", "desc"),
-    where("postId", "==", postId)
-  );
-  const res = await getDocs(q);
-  const data = res.docs.map((el) => el.data());
+    const userRef = collection(db, "user");
+    const userDocs = await getDocs(userRef);
+    const userList = userDocs.docs.map((el) => el.data());
 
-  const userRef = collection(db, "user");
-  const userDocs = await getDocs(userRef);
-  const userList = userDocs.docs.map((el) => el.data());
+    // 유저 db를 담을 Map 생성
+    const userMap = new Map();
+    // 사용자 데이터 목록을 반복하면서 Map에 사용자 UID를 키로 하여 데이터를 저장
+    for (const userData of userList) {
+      userMap.set(userData.uid, userData);
+    }
+    // 게시물 데이터 목록을 반복하면서 사용자 데이터를 연결
+    for (let i = 0; i < data.length; i++) {
+      const userData = userMap.get(data[i].uid);
+      // 기존 data에 displayName과 photoURL을 추가
+      if (userData) {
+        data[i].displayName = userData.displayName;
+        data[i].photoURL = userData.photoURL;
+      }
+    }
 
-  // 유저 db를 담을 Map 생성
-  const userMap = new Map();
-  // 사용자 데이터 목록을 반복하면서 Map에 사용자 UID를 키로 하여 데이터를 저장
-  for (const userData of userList) {
-    userMap.set(userData.uid, userData);
+    return {commentDoc, data: data as ICommentData[]};
+  } catch (error) {
+    console.error(error);
+    throw error;
   }
+};
 
-  // 게시물 데이터 목록을 반복하면서 사용자 데이터를 연결
-  for (let i = 0; i < data.length; i++) {
-    const userData = userMap.get(data[i].uid);
-    // 기존 data에 displayName과 photoURL을 추가
-    data[i].displayName = userData.displayName;
-    data[i].photoURL = userData.photoURL;
+/**
+ * 댓글 페이징
+ */
+export const fetchPagingCommentData = async (
+  page: QueryDocumentSnapshot<DocumentData, DocumentData>,
+  postId: string, pagePerData:number
+) => {
+  try {
+    const commentsRef = collection(db, "comments");
+    const q = query(
+      commentsRef,
+      orderBy("createdAt", "desc"),
+      where("postId", "==", postId),
+      startAfter(page),
+      limit(pagePerData),
+    );
+    const commentDoc = await getDocs(q);
+    const data = commentDoc.docs.map((el) => el.data());
+
+    const userRef = collection(db, "user");
+    const userDocs = await getDocs(userRef);
+    const userList = userDocs.docs.map((el) => el.data());
+
+    // 유저 db를 담을 Map 생성
+    const userMap = new Map();
+    // 사용자 데이터 목록을 반복하면서 Map에 사용자 UID를 키로 하여 데이터를 저장
+    for (const userData of userList) {
+      userMap.set(userData.uid, userData);
+    }
+    // 게시물 데이터 목록을 반복하면서 사용자 데이터를 연결
+    for (let i = 0; i < data.length; i++) {
+      const userData = userMap.get(data[i].uid);
+      // 기존 data에 displayName과 photoURL을 추가
+      if (userData) {
+        data[i].displayName = userData.displayName;
+        data[i].photoURL = userData.photoURL;
+      }
+    }
+
+    return {commentDoc, data: data as ICommentData[]};
+  } catch (error) {
+    console.error(error);
+    throw error;
   }
-
-  return data as ICommentData[];
 };
 
 /**
  * 댓글 추가
  */
 export const fetchAddComment = async (commentData: ICommentData) => {
-  // comment colledcion에 댓글 doc 추가
-  const commentsRef = collection(db, "comments");
-  const addCommentPromise = setDoc(
-    doc(commentsRef, commentData.commentId),
-    commentData
-  );
-  
-  // 게시물 doc에 commenCount 늘리기
-  const postDoc = doc(db, `post/${commentData.postId}`);
-  const addCommentCountPromise = updateDoc(postDoc, {
-    commentCount: increment(1)
-  });
+  try {
+    // comment colledcion에 댓글 doc 추가
+    const commentsRef = collection(db, "comments");
+    const addCommentPromise = setDoc(
+      doc(commentsRef, commentData.commentId),
+      commentData
+    );
 
-  await Promise.all([addCommentPromise, addCommentCountPromise]);
+    // 게시물 doc에 commenCount 늘리기
+    const postDoc = doc(db, `post/${commentData.postId}`);
+    const addCommentCountPromise = updateDoc(postDoc, {
+      commentCount: increment(1)
+    });
+
+    await Promise.all([addCommentPromise, addCommentCountPromise]);
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 };
 
 /**
@@ -77,8 +142,13 @@ export const fetchAddComment = async (commentData: ICommentData) => {
 export const fetchEditComment = async (
   commentEditData: Pick<ICommentData, "commentId" | "content">
 ) => {
-  const commentDoc = doc(db, `comments/${commentEditData.commentId}`);
-  await updateDoc(commentDoc, { content: commentEditData.content });
+  try {
+    const commentDoc = doc(db, `comments/${commentEditData.commentId}`);
+    await updateDoc(commentDoc, { content: commentEditData.content });
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 };
 
 /**
@@ -87,17 +157,59 @@ export const fetchEditComment = async (
 export const fetchRemoveComment = async (
   commentData: ICommentData
 ): Promise<void> => {
-  // 댓글 삭제
-  const commentsDoc = doc(db, `comments/${commentData.commentId}`);
-  const removeCommentPromise = deleteDoc(commentsDoc);
-  
-  // 게시물 doc에서 commentCount 줄이기
-  const postDoc = doc(db, `post/${commentData.postId}`);
-  const removeCommentCountPromise = updateDoc(postDoc, {
-    commentCount: increment(-1)
-  });
+  try {
+    // 댓글 삭제
+    const commentsDoc = doc(db, `comments/${commentData.commentId}`);
+    const removeCommentPromise = deleteDoc(commentsDoc);
 
-  await Promise.all([removeCommentPromise, removeCommentCountPromise]);
+    // 게시물 doc에서 commentCount 줄이기
+    const postDoc = doc(db, `post/${commentData.postId}`);
+    const removeCommentCountPromise = updateDoc(postDoc, {
+      commentCount: increment(-1)
+    });
+    await Promise.all([removeCommentPromise, removeCommentCountPromise]);
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
 };
 
+/**
+ * 댓글 신고
+ */
 
+export const fetchReportComment = async (
+  reportCommentData: Pick<ICommentData, "commentId" | "reportCount"> & {
+    postId: string;
+  }
+) => {
+  try {
+    const commentDoc = doc(db, `comments/${reportCommentData.commentId}`);
+    const addReportCountPromise = await updateDoc(commentDoc, {
+      reportCount: increment(1),
+      isBlock: reportCommentData.reportCount >= 4 ? true : false
+    });
+
+    const decreaseCommentCountPromise = []
+    if (reportCommentData.reportCount >= 4) {
+      const postDoc = doc(db, `post/${reportCommentData.postId}`);
+      decreaseCommentCountPromise.push(updateDoc(postDoc, {
+        commentCount: increment(-1)
+      }));
+    }
+    if (!auth.currentUser) return;
+    const userDoc = doc(db, `user/${auth.currentUser.uid}`);
+    const addReportListPromise = updateDoc(userDoc, {
+      reportCommentList: arrayUnion(reportCommentData.commentId)
+    });
+
+    await Promise.all([
+      addReportCountPromise,
+      decreaseCommentCountPromise,
+      addReportListPromise
+    ]);
+  } catch (error) {
+    console.error(error);
+    throw error;
+  }
+};
