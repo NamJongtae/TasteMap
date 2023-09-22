6f6a879c33a97dfda572b0266cacf144f16237a8
MAIN6419 <dark9737@gmail.com>
2023-09-22 17:38:22 +0900

    Feat: 무한 스크롤 로딩 시 사용할 ScrollLoading 컴포넌트 구현

---
+++ src/compoent/commons/loading/ScrollLoading.styles.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const LoadingWrpper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  height: 100px;
+`;
+export const LoadingImg = styled.img``;
---
+++ src/compoent/commons/loading/ScrollLoading.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+import { LoadingWrpper, LoadingImg } from './ScrollLoading.styles';
+
+
+export default function ScrollLoading() {
+  return (
+    <LoadingWrpper>
+      <LoadingImg src='/assets/scrollSpinner.gif' />
+    </LoadingWrpper>
+  );
+}
