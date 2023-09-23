bd3621714b50c783a06951470a22dde1e4b838e7
MAIN6419 <dark9737@gmail.com>
2023-09-23 22:41:50 +0900

    Feat: profileSlice 유저 프로필 게시물 관련 API 및 팔로우/언팔로우 API redux-thunk를 통해 비동기 처리 및 initalState 추가, reducer 추가 유저 팔로우 함수 비동기 처리인 thunkFetchFollow, 유저 언팔로우 함수 비동기 처리인 thunkFetchUnfollow, 유저 프로필 게시물 첫 페이지 조회 함수 비동기 처리인 thunkFetchProfileFirstPageData, 유저 프로필 게시물 페이징 함수 비동기 처리인 thunkFetchProfilePagingData 추가 initalState 자신의 유저 프로필 데이터ProfileData에서  myProfileData로 state명 수정, 현재 프로필 페이지의 유저 프로필 데이터 userProfileData, 프로필 게시물 데이터 profilePostListData, 프로필 게시물 페이지 page, 다음 게시물의 유뮤인 hasMore, 페이지 당 데이터 수인pagePerData, 로딩 유뮤 isLoading 추가 reducer 자신의 프로필 데이터를 업데이트하는 setMyProfile, 현재 페이지의 유저 프로필을 업데이트하는 setUserProfile, 유저 프로필 게시물을 업데이트 하는 setProfilePostListData 추가

--- src/slice/profileSlice.ts
+++ src/slice/profileSlice.ts
@@ -1,38 +1,278 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
-import { IKnownError, IProfileData } from "../api/apiType";
-import { fetchProfile } from "../api/firebase/profileAPI";
+import { IKnownError, IPostData, IProfileData } from "../api/apiType";
+import {
+  fetchFollow,
+  fetchProfile,
+  fetchProfileFirstPageData,
+  fetchProfilePagingData,
+  fetchUnfollow
+} from "../api/firebase/profileAPI";
 import { sweetToast } from "../library/sweetAlert/sweetAlert";
+import {
+  DocumentData,
+  QueryDocumentSnapshot,
+  QuerySnapshot
+} from "firebase/firestore";
 
-export const thunkFetchUserProfile = createAsyncThunk<
+/**
+ * 자신의 프로필 조회
+ */
+export const thunkFetchMyProfile = createAsyncThunk<
   IProfileData | undefined,
-  void,
+  string,
   { rejectValue: IKnownError }
->("profileSlice/thunkFetchUserProfile", async (_, thuckAPI) => {
+>("profileSlice/thunkFetchMyProfile", async (uid, thuckAPI) => {
   try {
-    const res = await fetchProfile();
+    const res = await fetchProfile(uid);
     return res;
   } catch (error: any) {
     return thuckAPI.rejectWithValue(error);
   }
 });
 
+/**
+ * 유저의 프로필 조회
+ */
+export const thunkFetchUserProfile = createAsyncThunk<
+  IProfileData | undefined,
+  string,
+  { rejectValue: IKnownError }
+>("profileSlice/thunkFetchUserProfile", async (uid, thuckAPI) => {
+  try {
+    const res = await fetchProfile(uid);
+    return res;
+  } catch (error: any) {
+    return thuckAPI.rejectWithValue(error);
+  }
+});
+
+/**
+ * 유저 팔로우
+ */
+export const thunkFetchFollow = createAsyncThunk<
+  { myUid: string; userUid: string } | undefined,
+  { myUid: string; userUid: string },
+  { rejectValue: IKnownError }
+>("profileSlice/thunkFetchFollow", async ({ myUid, userUid }, thuckAPI) => {
+  try {
+    await fetchFollow(myUid, userUid);
+    return { myUid, userUid };
+  } catch (error: any) {
+    return thuckAPI.rejectWithValue(error);
+  }
+});
+
+/**
+ * 유저 언팔로우
+ */
+export const thunkFetchUnfollow = createAsyncThunk<
+  { myUid: string; userUid: string } | undefined,
+  { myUid: string; userUid: string },
+  { rejectValue: IKnownError }
+>("profileSlice/thunkFetchUnfollow", async ({ myUid, userUid }, thuckAPI) => {
+  try {
+    await fetchUnfollow(myUid, userUid);
+    return { myUid, userUid };
+  } catch (error: any) {
+    return thuckAPI.rejectWithValue(error);
+  }
+});
+
+/**
+ * 유저 프로필 게시물 첫 페이지 조회
+ */
+export const thunkFetchProfileFirstPageData = createAsyncThunk<
+  | { postDocs: QuerySnapshot<DocumentData, DocumentData>; data: IPostData[] }
+  | undefined,
+  { uid: string; pagePerData: number },
+  { rejectValue: IKnownError }
+>(
+  "profileSlice/thunkFetchProfileFirstPageData",
+  async ({ uid, pagePerData }, thuckAPI) => {
+    try {
+      const res = await fetchProfileFirstPageData(uid, pagePerData);
+      return res;
+    } catch (error: any) {
+      thuckAPI.rejectWithValue(error);
+    }
+  }
+);
+
+/**
+ * 유저 프로필 게시물 페이징
+ */
+export const thunkFetchProfilePagingData = createAsyncThunk<
+  | {
+      postDocs: QuerySnapshot<DocumentData, DocumentData>;
+      data: IPostData[];
+    }
+  | undefined,
+  {
+    uid: string;
+    pagePerData: number;
+    page: QueryDocumentSnapshot<DocumentData, DocumentData>;
+  },
+  { rejectValue: IKnownError }
+>(
+  "profileSlice/thunkFetchProfilePagingData",
+  async ({ uid, pagePerData, page }, thunkAPI) => {
+    try {
+      const res = await fetchProfilePagingData(uid, pagePerData, page);
+      return res;
+    } catch (error: any) {
+      thunkAPI.rejectWithValue(error);
+    }
+  }
+);
+
 export const profileSlice = createSlice({
   name: "profileSlice",
   initialState: {
-    profileData: {} as IProfileData,
+    myProfileData: {} as IProfileData,
+    userProfileData: {} as IProfileData,
+    profilePostListData: [] as IPostData[],
+    page: {} as QueryDocumentSnapshot<DocumentData>,
+    hasMore: false,
+    pagePerData: 10,
+    isLoading: false,
     error: ""
   },
   reducers: {
-    setprofile: (state, action) => {
-      state.profileData = action.payload;
-    }
+    setMyprofile: (state, action) => {
+      state.myProfileData = action.payload;
+    },
+    setUserProfile: (state, action) => {
+      state.myProfileData = action.payload;
+    },
+    setProfilePostListData: (state, action) => {
+      state.profilePostListData = action.payload;
+    },
   },
   extraReducers: (builder) => {
+    // 자신의 프로필 데이터 조회
+    builder.addCase(thunkFetchMyProfile.fulfilled, (state, action) => {
+      if (action.payload) state.myProfileData = action.payload;
+    });
+    builder.addCase(thunkFetchMyProfile.rejected, (state, action) => {
+      if (action.payload) state.error = action.payload.message;
+      sweetToast(
+        "알 수 없는 에러가 발생하였습니다.\n잠시 후 다시 시도해 주세요.",
+        "warning"
+      );
+      console.error(state.error);
+    });
+
+    // 유저의 프로필 데이터 조회
     builder.addCase(thunkFetchUserProfile.fulfilled, (state, action) => {
-      if (action.payload) state.profileData = action.payload;
+      if (action.payload) state.userProfileData = action.payload;
     });
     builder.addCase(thunkFetchUserProfile.rejected, (state, action) => {
       if (action.payload) state.error = action.payload.message;
+      sweetToast(
+        "알 수 없는 에러가 발생하였습니다.\n잠시 후 다시 시도해 주세요.",
+        "warning"
+      );
+      console.error(state.error);
+    });
+
+    // 유저 팔로우
+    builder.addCase(thunkFetchFollow.fulfilled, (state, action) => {
+      if (action.payload) {
+        const newData = { ...state.myProfileData };
+        newData.followerList?.push(action.payload.userUid);
+        state.myProfileData = newData;
+
+        const newUserProfile = { ...state.userProfileData };
+        newUserProfile.followingList?.push(action.payload.myUid);
+        state.userProfileData = newUserProfile;
+      }
+    });
+    builder.addCase(thunkFetchFollow.rejected, (state, action) => {
+      if (action.payload) state.error = action.payload.message;
+      sweetToast(
+        "알 수 없는 에러가 발생하였습니다.\n잠시 후 다시 시도해 주세요.",
+        "warning"
+      );
+      console.error(state.error);
+    });
+
+    // 유저 언팔로우
+    builder.addCase(thunkFetchUnfollow.fulfilled, (state, action) => {
+      if (action.payload) {
+        const newData = { ...state.myProfileData };
+        newData.followerList = newData.followingList?.filter(
+          (item) => item !== action.payload?.userUid
+        );
+        state.myProfileData = newData;
+
+        const newUserProfile = { ...state.userProfileData };
+        newUserProfile.followingList = newUserProfile.followingList?.filter(
+          (item) => item !== action.payload?.myUid
+        );
+        state.userProfileData = newUserProfile;
+      }
+    });
+
+    builder.addCase(thunkFetchUnfollow.rejected, (state, action) => {
+      if (action.payload) state.error = action.payload.message;
+      sweetToast(
+        "알 수 없는 에러가 발생하였습니다.\n잠시 후 다시 시도해 주세요.",
+        "warning"
+      );
+      console.error(state.error);
+    });
+
+    // 프로필 게시물 데이터 첫 페이지
+    builder.addCase(thunkFetchProfileFirstPageData.pending, (state) => {
+      document.body.style.overflow = "hidden";
+      state.isLoading = true;
+    });
+    builder.addCase(
+      thunkFetchProfileFirstPageData.fulfilled,
+      (state, action) => {
+        document.body.style.overflow = "auto";
+        state.isLoading = false;
+        if (action.payload) {
+          state.profilePostListData = action.payload?.data;
+          state.hasMore =
+            (action.payload?.data as IPostData[]).length % state.pagePerData ===
+            0;
+          state.page =
+            action.payload.postDocs.docs[
+              action.payload.postDocs.docs.length - 1
+            ];
+        }
+      }
+    );
+    builder.addCase(
+      thunkFetchProfileFirstPageData.rejected,
+      (state, action) => {
+        if (!action.payload) return;
+        state.isLoading = false;
+        state.error = action.payload.message;
+        sweetToast(
+          "알 수 없는 에러가 발생하였습니다.\n잠시 후 다시 시도해 주세요.",
+          "warning"
+        );
+        console.error(state.error);
+      }
+    );
+
+    // 게시물 페이징
+    builder.addCase(thunkFetchProfilePagingData.fulfilled, (state, action) => {
+      if (!action.payload) return;
+      state.profilePostListData = [
+        ...state.profilePostListData,
+        ...(action.payload?.data as IPostData[])
+      ];
+      state.page =
+        action.payload.postDocs.docs[action.payload.postDocs.docs.length - 1];
+      state.hasMore = action.payload.data.length % state.pagePerData === 0;
+    });
+    builder.addCase(thunkFetchProfilePagingData.rejected, (state, action) => {
+      if (!action.payload) return;
+      state.isLoading = false;
+      state.error = action.payload.message;
       sweetToast(
         "알 수 없는 에러가 발생하였습니다.\n잠시 후 다시 시도해 주세요.",
         "warning"
