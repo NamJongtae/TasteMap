d272ae300fcae7c66f92144ea4e466b8bba9ee77
MAIN6419 <dark9737@gmail.com>
2023-09-25 00:55:20 +0900

    Fix: Home 컴포넌트 PostList 컴포넌트 isProfile props명을 isProfilePage로 수정

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     <>
     <Header type='home'/>
       <Wrapper>
-        <PostList isProfile={false}/>
+        <PostList isProfilePage={false}/>
       </Wrapper>
     </>
   );
