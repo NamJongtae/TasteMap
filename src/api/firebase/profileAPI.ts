d9827d6ab09855c39a9572eec2cf7578fa2c1955
MAIN6419 <dark9737@gmail.com>
2023-09-25 00:02:08 +0900

    Feat: profileAPI 수정 및 follower, following 무한 스크롤을 위한 API 함수 구현 유저 프로필 게시물 관련 api 함수 시간순 정렬 쿼리 추가 팔로우/언팔로우 api 함수에서 팔로워/팔로잉 유저 데이터를 무한 스크롤로 구현하기 위해 follower, following subColletcion에 팔로우 doc를 생성,제거하는 로직 추가 팔로워/팔로잉 첫 페이지 데이터를 조회하는 fetchFirstpageFollowData/fetchFirstPageFollowingData api 함수 구현, 팔로워/팔로잉 페이지에 따른 데이터를 조회하는 fetchPagingFollowerData/fetchPagingFollowingData api 함수 구현

--- src/api/firebase/profileAPI.ts
+++ src/api/firebase/profileAPI.ts
@@ -1,8 +1,6 @@
 import {
   DocumentData,
   QueryDocumentSnapshot,
-  arrayRemove,
-  arrayUnion,
   collection,
   doc,
   getDoc,
@@ -11,10 +9,15 @@ import {
   query,
   startAfter,
   updateDoc,
-  where
+  where,
+  orderBy,
+  setDoc,
+  deleteDoc,
+  arrayUnion,
+  arrayRemove,
 } from "firebase/firestore";
 import { db } from "./setting";
-import { IProfileData, IPostData, IUserData } from "../apiType";
+import { IProfileData, IPostData, IUserData, IFollowData } from "../apiType";
 
 /**
  * 유저 프로필 조회
@@ -42,7 +45,12 @@ export const fetchProfileFirstPageData = async (
 ) => {
   try {
     const postRef = collection(db, `post`);
-    const q = query(postRef, where("uid", "==", uid), limit(pagePerData));
+    const q = query(
+      postRef,
+      where("uid", "==", uid),
+      orderBy("createdAt", "desc"),
+      limit(pagePerData)
+    );
     const postDocs = await getDocs(q);
     const data = postDocs.docs.map((el) => el.data());
 
@@ -85,6 +93,7 @@ export const fetchProfilePagingData = async (
     const q = query(
       postRef,
       where("uid", "==", uid),
+      orderBy("createdAt", "desc"),
       startAfter(page),
       limit(pagePerData)
     );
@@ -122,33 +131,194 @@ export const fetchProfilePagingData = async (
 export const fetchFollow = async (myUid: string, userUid: string) => {
   // 나의 프로필 Doc에 followList에 해당 유저의 uid 추가
   const myProfileDoc = doc(db, `user/${myUid}`);
+  const folloerListRef = collection(myProfileDoc, `follower`);
+  const addFollowerListDocPromise = setDoc(doc(folloerListRef, userUid), {
+    uid: userUid
+  });
   const addFollowerListPromise = updateDoc(myProfileDoc, {
     followerList: arrayUnion(userUid)
   });
 
   // 팔로우한 유저 프로필 Doc에 followingList에 나의 uid 추가
   const userProfileDoc = doc(db, `user/${userUid}`);
+  const followingListRef = collection(userProfileDoc, `following`);
+  const addFollowingListDocPromise = setDoc(doc(followingListRef, myUid), {
+    uid: myUid
+  });
   const addFollowingListPromise = updateDoc(userProfileDoc, {
     followingList: arrayUnion(myUid)
   });
 
-  await Promise.all([addFollowerListPromise, addFollowingListPromise]);
+  await Promise.all([
+    addFollowerListPromise,
+    addFollowingListPromise,
+    addFollowerListDocPromise,
+    addFollowingListDocPromise
+  ]);
 };
+
 /**
  * 언팔로우
  */
 export const fetchUnfollow = async (myUid: string, userUid: string) => {
   // 나의 프로필 Doc에 followList에 해당 유저의 uid 제거
-  const userDoc = doc(db, `user/${myUid}`);
-  const removeFollowerListPromise = updateDoc(userDoc, {
+  const myProfileDoc = doc(db, `user/${myUid}`);
+  const followerDoc = doc(myProfileDoc, `follower/${userUid}`);
+  const removeFollowerDocPromise = deleteDoc(followerDoc);
+  const removeFollowerListPromise = updateDoc(myProfileDoc, {
     followerList: arrayRemove(userUid)
   });
 
- // 팔로우한 유저 프로필 Doc에 followingList에 나의 uid 제거
+  // 팔로우한 유저 프로필 Doc에 followingList에 나의 uid 제거
   const userProfileDoc = doc(db, `user/${userUid}`);
+  const followingDoc = doc(userProfileDoc, `following/${myUid}`);
+  const removeFollowingDocPromise = deleteDoc(followingDoc);
   const removeFollowingListPromise = updateDoc(userProfileDoc, {
     followingList: arrayRemove(myUid)
   });
 
-  await Promise.all([removeFollowerListPromise, removeFollowingListPromise]);
+  await Promise.all([
+    removeFollowerDocPromise,
+    removeFollowerListPromise,
+    removeFollowingDocPromise,
+    removeFollowingListPromise
+  ]);
+};
+
+/**
+ * 팔로워 첫 페이지 목록 조회
+ */
+export const fetchFirstpageFollowerData = async (
+  uid: string,
+  pagePerData: number
+) => {
+  const userDoc = doc(db, `user/${uid}`);
+  const followerRef = collection(userDoc, "follower");
+  const q = query(followerRef, limit(pagePerData));
+  const followerDocs = await getDocs(q);
+  const data = followerDocs.docs.map((el) => el.data());
+
+  if (data.length > 0) {
+    const userRef = collection(db, "user");
+    const userUid: string[] = [...data].map((item) => item.uid);
+    const userQuery = query(userRef, where("uid", "in", userUid));
+    const res = await getDocs(userQuery);
+    const uidData: IUserData[] = res.docs.map((el) => {
+      return { uid: el.id, ...el.data() };
+    });
+
+    for (let i = 0; i < data.length; i++) {
+      const userData = uidData.find((userData) => userData.uid === data[i].uid);
+      if (userData) {
+        data[i].displayName = userData.displayName;
+        data[i].photoURL = userData.photoURL;
+      }
+    }
+  }
+
+  return { followerDocs, data: data as IFollowData[] };
+};
+
+/**
+ * 팔로워 페이징
+ */
+export const fetchPagingFollowerData = async (
+  uid: string,
+  page: QueryDocumentSnapshot<DocumentData, DocumentData>,
+  pagePerData: number
+) => {
+  const userDoc = doc(db, `user/${uid}`);
+  const followerRef = collection(userDoc, "follower");
+  const q = query(followerRef, startAfter(page), limit(pagePerData));
+  const followerDocs = await getDocs(q);
+  const data = followerDocs.docs.map((el) => el.data());
+
+  if (data.length > 0) {
+    const userRef = collection(db, "user");
+    const userUid: string[] = [...data].map((item) => item.uid);
+    const userQuery = query(userRef, where("uid", "in", userUid));
+    const res = await getDocs(userQuery);
+    const uidData: IUserData[] = res.docs.map((el) => {
+      return { uid: el.id, ...el.data() };
+    });
+
+    for (let i = 0; i < data.length; i++) {
+      const userData = uidData.find((userData) => userData.uid === data[i].uid);
+      if (userData) {
+        data[i].displayName = userData.displayName;
+        data[i].photoURL = userData.photoURL;
+      }
+    }
+  }
+
+  return { followerDocs, data: data as IFollowData[] };
+};
+
+/**
+ * 팔로잉 첫 페이지 목록 조회
+ */
+export const fetchFirstpageFollowingData = async (
+  uid: string,
+  pagePerData: number
+) => {
+  const userDoc = doc(db, `user/${uid}`);
+  const followingRef = collection(userDoc, "following");
+  const q = query(followingRef, limit(pagePerData));
+  const followingDocs = await getDocs(q);
+  const data = followingDocs.docs.map((el) => el.data());
+
+  if (data.length > 0) {
+    const userRef = collection(db, "user");
+    const userUid: string[] = [...data].map((item) => item.uid);
+    const userQuery = query(userRef, where("uid", "in", userUid));
+    const res = await getDocs(userQuery);
+    const uidData: IUserData[] = res.docs.map((el) => {
+      return { uid: el.id, ...el.data() };
+    });
+
+    for (let i = 0; i < data.length; i++) {
+      const userData = uidData.find((userData) => userData.uid === data[i].uid);
+      if (userData) {
+        data[i].displayName = userData.displayName;
+        data[i].photoURL = userData.photoURL;
+      }
+    }
+  }
+
+  return { followingDocs, data: data as IFollowData[] };
+};
+
+/**
+ * 팔로잉 페이징
+ */
+export const fetchPagingFollowingData = async (
+  uid: string,
+  page: QueryDocumentSnapshot<DocumentData, DocumentData>,
+  pagePerData: number
+) => {
+  const userDoc = doc(db, `user/${uid}`);
+  const followingRef = collection(userDoc, "follower");
+  const q = query(followingRef, startAfter(page), limit(pagePerData));
+  const followingDocs = await getDocs(q);
+  const data = followingDocs.docs.map((el) => el.data());
+
+  if (data.length > 0) {
+    const userRef = collection(db, "user");
+    const userUid: string[] = [...data].map((item) => item.uid);
+    const userQuery = query(userRef, where("uid", "in", userUid));
+    const res = await getDocs(userQuery);
+    const uidData: IUserData[] = res.docs.map((el) => {
+      return { uid: el.id, ...el.data() };
+    });
+
+    for (let i = 0; i < data.length; i++) {
+      const userData = uidData.find((userData) => userData.uid === data[i].uid);
+      if (userData) {
+        data[i].displayName = userData.displayName;
+        data[i].photoURL = userData.photoURL;
+      }
+    }
+  }
+
+  return { followingDocs, data: data as IFollowData[] };
 };
