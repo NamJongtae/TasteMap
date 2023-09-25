a57ceccee089cb77bbebed1c145f8c804890d6a1
MAIN6419 <dark9737@gmail.com>
2023-09-25 20:10:38 +0900

    Fix: postSlice 삭제한 게시물 조회 비동기 처리 함수 thuckFetchPostData 복구

--- src/slice/postSlice.ts
+++ src/slice/postSlice.ts
@@ -304,6 +304,27 @@ export const postSlice = createSlice({
     }
   },
   extraReducers: (builder) => {
+    // 게시물 데이터 조회
+    builder.addCase(thuckFetchPostData.pending, (state) => {
+      document.body.style.overflow = "hidden";
+      state.isLoading = true;
+    });
+    builder.addCase(thuckFetchPostData.fulfilled, (state, action) => {
+      if (action.payload) state.postData = action.payload;
+      document.body.style.overflow = "auto";
+      state.isLoading = false;
+    });
+    builder.addCase(thuckFetchPostData.rejected, (state, action) => {
+      if (action.payload) state.error = action.payload.message;
+      document.body.style.overflow = "auto";
+      state.isLoading = false;
+      sweetToast(
+        "알 수 없는 에러가 발생하였습니다.\n잠시 후 다시 시도해 주세요.",
+        "warning"
+      );
+      console.error(state.error);
+    });
+
     // 맛집 지도 검색
     builder.addCase(thuckFetchSearchMap.fulfilled, (state, action) => {
       state.searchMapData = action.payload;
