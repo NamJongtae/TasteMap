fbd3881c646c8fe9341863792144cdbb82ccf0cb
MAIN6419 <dark9737@gmail.com>
2023-10-01 15:10:29 +0900

    Fix: SearchModal 컴포넌트 모달창이 열릴시 input에 focus가도록 useEffect hook 추가. SearchItem key값을 item.address로 변경

--- src/pages/postUpload/SearchModal.tsx
+++ src/pages/postUpload/SearchModal.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import {
   Wrapper,
   Dim,
@@ -25,7 +25,7 @@ import {
 } from "./searchModal.styles";
 import { useDispatch, useSelector } from "react-redux";
 import { AppDispatch, RootState } from "../../store/store";
-import { postSlice, thuckFetchSearchMap } from "../../slice/postSlice";
+import { postSlice, thunkFetchSearchMap } from "../../slice/postSlice";
 import { ISearchMapData } from "../../api/apiType";
 import { sweetToast } from "../../library/sweetAlert/sweetAlert";
 
@@ -39,6 +39,7 @@ export default function SearchModal({ closeSearchModal }: IProps) {
     (state: RootState) => state.post.searchMapData
   );
   const dispatch = useDispatch<AppDispatch>();
+  const inputRef = useRef<HTMLInputElement>(null);
   // 검색어
   const [inputValue, setInputValue] = useState("");
   // 검색을 했는지 초기상태를 체크 초기 툴팁을 표시하기위해
@@ -58,8 +59,8 @@ export default function SearchModal({ closeSearchModal }: IProps) {
     e.preventDefault();
     // 검색어가 없는 경우 return 처리
     if (!inputValue) return sweetToast("검색어를 입력해주세요.", "warning");
-    // redux thuck를 이용해 비동기 처리 서버로 부터 검색한 내용을 받아옴
-    await dispatch(thuckFetchSearchMap(inputValue));
+    // redux thunk를 이용해 비동기 처리 서버로 부터 검색한 내용을 받아옴
+    await dispatch(thunkFetchSearchMap(inputValue));
     setSearchKeyword(inputValue);
     setIsSearch(true);
   };
@@ -69,6 +70,7 @@ export default function SearchModal({ closeSearchModal }: IProps) {
   const onClickSelected = (item: ISearchMapData) => {
     // selectedMapData 상태 업데이트
     dispatch(postSlice.actions.setSelectedMapData(item));
+
     // searchMapData 상태 리셋
     dispatch(postSlice.actions.resetSearchMapData());
     // 검색 input값 초기화
@@ -76,6 +78,12 @@ export default function SearchModal({ closeSearchModal }: IProps) {
     // 모달 창 닫기
     closeSearchModal();
   };
+
+  useEffect(() => {
+    if (inputRef.current) {
+      inputRef.current.focus();
+    }
+  }, []);
 
   return (
     <Wrapper>
@@ -90,21 +98,22 @@ export default function SearchModal({ closeSearchModal }: IProps) {
             value={inputValue}
             onChange={onChangeValue}
             placeholder='가게명, 상호명 검색'
+            ref={inputRef}
           />
         </SearchInputForm>
         {isSarch ? (
           searchMapData.length > 0 ? (
             <SearchList>
-              {searchMapData.map((item) => {
+              {searchMapData.map((item, idx) => {
                 return (
-                  <SearchItem key={item.title}>
+                  <SearchItem key={item.address||"" + idx}>
                     <ItemList>
                       <Item>
                         <SearchItemTag>가게명</SearchItemTag>
                         <SearchItemText>{item.title}</SearchItemText>
                       </Item>
                       <Item>
-                        <SearchItemTag>도로명 </SearchItemTag>
+                        <SearchItemTag>도로명 주소</SearchItemTag>
                         <SearchItemText>{item.roadAddress}</SearchItemText>
                       </Item>
                       <Item>
