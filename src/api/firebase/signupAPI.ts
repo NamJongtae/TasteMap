05420ba742ec8445b20030481ddc264180a37854
MAIN6419 <dark9737@gmail.com>
2023-10-06 22:10:59 +0900

    Fix: signupAPI fetchSignup 함수에서 회원가입 후 로그인 메세지를 띄우도록 sweetToast 추가

--- src/api/firebase/signupAPI.ts
+++ src/api/firebase/signupAPI.ts
@@ -11,6 +11,7 @@ import {
   ref as storageRef
 } from "firebase/storage";
 import { v4 as uuidv4 } from "uuid";
+import { sweetToast } from "../../library/sweetAlert/sweetAlert";
 
 const auth = getAuth();
 // 회원가입 API
@@ -24,6 +25,13 @@ export const fetchSignup = async (
 ) => {
   try {
     const res = await createUserWithEmailAndPassword(auth, email, password);
+    if (auth.currentUser) {
+      sweetToast(
+        `${displayName}님 환영합니다.`,
+        "success",
+        1500
+      );
+    }
     const fileName = file && `${uuidv4()}_${file.name}`;
     const uploadImgURLRes =
       file &&
@@ -32,13 +40,17 @@ export const fetchSignup = async (
         file
       ));
     const photoURL =
-      file && uploadImgURLRes && (await getDownloadURL(uploadImgURLRes.ref)) || process.env.REACT_APP_DEFAULT_PROFILE_IMG;
+      (file &&
+        uploadImgURLRes &&
+        (await getDownloadURL(uploadImgURLRes.ref))) ||
+      process.env.REACT_APP_DEFAULT_PROFILE_IMG;
     await updateProfile(res.user, {
       displayName,
       photoURL
     });
-  
+
     const user = collection(db, "user");
+
     await setDoc(doc(user, `${res.user.uid}`), {
       uid: res.user.uid,
       email: res.user.email,
@@ -54,15 +66,15 @@ export const fetchSignup = async (
       introduce,
       createdAt: Timestamp.fromDate(new Date())
     });
+
     return {
       uid: res.user.uid,
       email: res.user.email,
       displayName: res.user.displayName,
       photoURL: res.user.photoURL || process.env.REACT_APP_DEFAULT_PROFILE_IMG
     };
-  } catch(error) {
+  } catch (error) {
     console.error(error);
     throw error;
   }
- 
 };
