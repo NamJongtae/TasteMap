1d0faf892ab56cfceb16b8036da500c820f09881
MAIN6419 <dark9737@gmail.com>
2023-11-28 19:39:49 +0900

    Fix: ShareTasetMap 컴포넌트 코드의 가독성 향상을 위해 JSX 조건부 렌더링 삼항연산자 대신 if문으로 처리

--- src/pages/shareTasteMap/ShareTasteMap.tsx
+++ src/pages/shareTasteMap/ShareTasteMap.tsx
@@ -56,117 +56,118 @@ export default function ShareTasteMap() {
     dispatch(tasteMapSlice.actions.setContentType(EMapContentType.LIST));
   };
 
+  const isInvalidMap = !userProfile?.storedMapList;
+
+  if (userIsFetching) {
+    return <Loading />;
+  }
+
+  if (isInvalidMap) {
+    return (
+      <InvaildMap>
+        <InvaildMapImg
+          src={resolveWebp("/assets/webp/icon-cloche.webp", "svg")}
+        />
+        <InvaildMapText>유효하지 않은 맛집 지도입니다.</InvaildMapText>
+      </InvaildMap>
+    );
+  }
+
   return (
     <>
-      {userIsFetching ? (
-        <Loading />
-      ) : !userProfile?.storedMapList ? (
-        <InvaildMap>
-          <InvaildMapImg
-            src={resolveWebp("/assets/webp/icon-cloche.webp", "svg")}
-          />
-          <InvaildMapText>유효하지 않은 맛집 지도입니다.</InvaildMapText>
-        </InvaildMap>
-      ) : (
+      <Helmet>
+        <meta
+          property='og:title'
+          content={userProfile.displayName + "님의 TasteMap"}
+        />
+        <meta property='og:type' content='webpsite' />
+        <meta
+          property='og:url'
+          content={"https://tasteMap.site/tasteMap/share" + userProfile!.uid}
+        />
+        <meta
+          property='og:image'
+          content='https://firebasestorage.googleapis.com/v0/b/tastemap-c9a2a.appspot.com/o/images%2Fog%2Fog-img.png?alt=media&token=d4503b8e-af9e-4d6b-9367-268973d3104d&_gl=1*179wgyn*_ga*MTY1NzkxNDYxOC4xNjg4NTU5ODMy*_ga_CW55HF8NVT*MTY5NjQyMTkyOS4yMDEuMS4xNjk2NDIxOTcyLjE3LjAuMA..'
+        />
+        <meta
+          property='og:description'
+          content={
+            (userProfile.displayName || "") + "님이 공유하는 나만의 맛집 지도"
+          }
+          data-react-helmet='true'
+        />
+      </Helmet>
+      <>
         <>
-          <Helmet>
-            <meta
-              property='og:title'
-              content={userProfile.displayName + "님의 TasteMap"}
-            />
-            <meta property='og:type' content='webpsite' />
-            <meta
-              property='og:url'
-              content={
-                "https://tasteMap.site/tasteMap/share" + userProfile!.uid
-              }
-            />
-            <meta
-              property='og:image'
-              content='https://firebasestorage.googleapis.com/v0/b/tastemap-c9a2a.appspot.com/o/images%2Fog%2Fog-img.png?alt=media&token=d4503b8e-af9e-4d6b-9367-268973d3104d&_gl=1*179wgyn*_ga*MTY1NzkxNDYxOC4xNjg4NTU5ODMy*_ga_CW55HF8NVT*MTY5NjQyMTkyOS4yMDEuMS4xNjk2NDIxOTcyLjE3LjAuMA..'
-            />
-            <meta
-              property='og:description'
-              content={
-                (userProfile.displayName || "") +
-                "님이 공유하는 나만의 맛집 지도"
-              }
-              data-react-helmet='true'
-            />
-          </Helmet>
-          <>
-            <>
-              {myInfo.uid && <Header type='profile' />}
-              <Wrapper>
-                <ContetnTypeBtnWrapper>
-                  <MapBtn
-                    onClick={onClickMapType}
-                    contentType={contentType}
-                    style={{ top: "0px" }}
-                  >
-                    지도
-                  </MapBtn>
-                  <ListBtn
-                    onClick={onClickListType}
-                    contentType={contentType}
-                    style={{ top: "0px" }}
-                  >
-                    목록
-                  </ListBtn>
-                </ContetnTypeBtnWrapper>
-                <Title>
-                  <TitleImg src={"/assets/icon-searchMap.svg"} alt='맛집지도' />
-                  {userProfile.displayName}님의 맛집 지도
-                </Title>
-                <KakaomapWrapper contentType={contentType}>
-                  <Desc>마커 클릭 시 맛집 정보가 지도 아래 표시 됩니다.</Desc>
-                  <Kakaomap
-                    items={userProfile.storedMapList}
-                    isTasteMapPage={true}
-                  />
-                </KakaomapWrapper>
-                {clickMarkerData.title && (
-                  <ItemSingleList contentType={contentType}>
-                    <Item>
-                      <ItemTag>가게명</ItemTag>
-                      <ItemText>{clickMarkerData.title}</ItemText>
-                    </Item>
-                    <Item>
-                      <ItemTag>도로명 주소</ItemTag>
-                      <ItemText>{clickMarkerData.roadAddress}</ItemText>
-                    </Item>
-                    <Item>
-                      <ItemTag>지번 주소</ItemTag>
-                      <ItemText>{clickMarkerData.address}</ItemText>
-                    </Item>
-                    <Item>
-                      <ItemTag>카테고리</ItemTag>
-                      <ItemText>{clickMarkerData.category}</ItemText>
-                    </Item>
-                    <Item>
-                      <ItemTag>홈페이지</ItemTag>
-                      <ItemLink
-                        to={clickMarkerData.link || "#"}
-                        target='
+          {myInfo.uid && <Header type='profile' />}
+          <Wrapper>
+            <ContetnTypeBtnWrapper>
+              <MapBtn
+                onClick={onClickMapType}
+                contentType={contentType}
+                style={{ top: "0px" }}
+              >
+                지도
+              </MapBtn>
+              <ListBtn
+                onClick={onClickListType}
+                contentType={contentType}
+                style={{ top: "0px" }}
+              >
+                목록
+              </ListBtn>
+            </ContetnTypeBtnWrapper>
+            <Title>
+              <TitleImg src={"/assets/icon-searchMap.svg"} alt='맛집지도' />
+              {userProfile.displayName}님의 맛집 지도
+            </Title>
+            <KakaomapWrapper contentType={contentType}>
+              <Desc>마커 클릭 시 맛집 정보가 지도 아래 표시 됩니다.</Desc>
+              <Kakaomap
+                items={userProfile.storedMapList}
+                isTasteMapPage={true}
+              />
+            </KakaomapWrapper>
+            {clickMarkerData.title && (
+              <ItemSingleList contentType={contentType}>
+                <Item>
+                  <ItemTag>가게명</ItemTag>
+                  <ItemText>{clickMarkerData.title}</ItemText>
+                </Item>
+                <Item>
+                  <ItemTag>도로명 주소</ItemTag>
+                  <ItemText>{clickMarkerData.roadAddress}</ItemText>
+                </Item>
+                <Item>
+                  <ItemTag>지번 주소</ItemTag>
+                  <ItemText>{clickMarkerData.address}</ItemText>
+                </Item>
+                <Item>
+                  <ItemTag>카테고리</ItemTag>
+                  <ItemText>{clickMarkerData.category}</ItemText>
+                </Item>
+                <Item>
+                  <ItemTag>홈페이지</ItemTag>
+                  <ItemLink
+                    to={clickMarkerData.link || "#"}
+                    target='
               _blank'
-                      >
-                        {clickMarkerData.link}
-                      </ItemLink>
-                    </Item>
-                  </ItemSingleList>
-                )}
-                {contentType === EMapContentType.LIST && (
-                  <MyTasteMapList
-                    profile={userProfile || ({} as IUserProfileData)}
-                    items={userProfile!.storedMapList}
-                    isShareTasteMap={true}
-                  />
-                )}
-              </Wrapper>
-            </>
-          </>
+                  >
+                    {clickMarkerData.link}
+                  </ItemLink>
+                </Item>
+              </ItemSingleList>
+            )}
+            {contentType === EMapContentType.LIST && (
+              <MyTasteMapList
+                profile={userProfile || ({} as IUserProfileData)}
+                items={userProfile!.storedMapList}
+                isShareTasteMap={true}
+              />
+            )}
+          </Wrapper>
         </>
-      )}
+      </>
     </>
   );
 }
