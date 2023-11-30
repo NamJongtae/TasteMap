6cc219a4ca5a580cf4a24acb7d88e71baa5f4e39
MAIN6419 <dark9737@gmail.com>
2023-11-30 14:56:36 +0900

    Refactor: webp 지원여부를 관리하는 customhook 생성 및 기존 webpSupport.ts 삭제 webp 지원 여부 isWebpSupported를 redux에 저장하고 전역상태로 관리 및 localstroage에 저장하여 사용 기존 webpSupport 파일에서 사용하던 resolveWebp 함수를 customhook에 정의

---
+++ src/hook/useSupportedWebp.ts
@@ -0,0 +1,54 @@
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../store/store";
+import { settingSlice } from "../slice/settingSlice";
+import { useCallback, useEffect } from "react";
+
+export const useSupportedWebp = () => {
+  const disaptch = useDispatch<AppDispatch>();
+  // webp 지원여부
+  const isWebpSupported = useSelector(
+    (state: RootState) => state.setting.isWebpSupported
+  );
+  useEffect(() => {
+    // webp 지원여부 값이 없는 경우에만 실행
+    if (isWebpSupported === null) {
+      const image = new Image();
+      // 1px x 1px WebP 이미지
+      const webpdata =
+        "data:image/webp;base64,UklGRiQAAABXRUJQVlA4IBgAAAAwAQCdASoBAAEAAwA0JaQAA3AA/vuUAAA=";
+
+      const callback = (event: Event | string) => {
+        if (typeof event === "string") {
+          return console.error(event);
+        }
+        // event.type이 "load"인 경우와 이미지의 너비(image.width)가 1 픽셀인 경우를 검사하여 브라우저가 WebP 이미지를 지원하는지 여부를 판별
+        const result = event?.type === "load" && image.width === 1;
+        if (result) {
+          disaptch(settingSlice.actions.setIsWebpSupproted(true));
+        } else {
+          disaptch(settingSlice.actions.setIsWebpSupproted(false));
+        }
+        localStorage.setItem("webpSupported", JSON.stringify(result));
+      };
+
+      image.onerror = callback;
+      image.onload = callback;
+      image.src = webpdata;
+    }
+  }, [isWebpSupported]);
+
+  const resolveWebp = useCallback(
+    (img: string, fallbackExt: string) => {
+      // 이미지 포맷
+      const ext = img.split(".").pop();
+      // webpSupported false, ext가 webp인 경우
+      if (!isWebpSupported && ext === "webp") {
+        return img.replace("/webp", "").replace(".webp", `.${fallbackExt}`);
+      }
+      return img;
+    },
+    [isWebpSupported]
+  );
+
+  return { isWebpSupported, resolveWebp };
+};
