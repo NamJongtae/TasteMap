acfe3ddc31b5e9ef046ab0502ac82aff48114b1a
MAIN6419 <dark9737@gmail.com>
2023-12-11 20:53:42 +0900

    Fix: useUserProfileQuery isError 반환 값 추가 에러 처리를 위해 isError 반환 값을 추가

--- src/hook/query/profile/useUserProfileQuery.ts
+++ src/hook/query/profile/useUserProfileQuery.ts
@@ -2,12 +2,12 @@ import { useQuery } from "@tanstack/react-query";
 import { fetchUserProfile } from "../../../api/firebase/profileAPI";
 
 export const useUserProfileQuery = (uid: string) => {
-  const { data, isPending, isFetching } = useQuery({
+  const { data, isPending, isFetching, isError } = useQuery({
     queryKey: ["profile", uid],
     queryFn: async () => await fetchUserProfile(uid),
     enabled: !!uid,
-    retry: 1,
+    retry: 1
   });
 
-  return { data, isPending, isFetching };
+  return { data, isPending, isFetching, isError };
 };
