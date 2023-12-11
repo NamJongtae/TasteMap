32f42139c647f309c1a5b4e1c439fd5d0799fe0d
MAIN6419 <dark9737@gmail.com>
2023-12-11 21:09:36 +0900

    Fix: 컴포넌트가 언마운트 될 때 searchMapModal이 열려 있는 경우 닫히도록 clean-up 함수 추가

--- src/hook/logic/searchMapModal/useSearchMapModalController.ts
+++ src/hook/logic/searchMapModal/useSearchMapModalController.ts
@@ -1,13 +1,16 @@
 import { useCallback, useEffect, useRef } from "react";
 import { postSlice } from "../../../slice/postSlice";
-import { useDispatch } from "react-redux";
-import { AppDispatch } from "../../../store/store";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../../../store/store";
 import { searchSlice } from "../../../slice/searchSlice";
 import { useHistoryMobileBackBtn } from "../../useHistoryMobileBackBtn";
 
 export const useSearchMapModalController = () => {
   const dispatch = useDispatch<AppDispatch>();
   const closeBtnRef = useRef<HTMLButtonElement>(null);
+  const isOpenSearchMapModal = useSelector(
+    (state: RootState) => state.search.isOpenSearchMapModal
+  );
 
   /**
    * 검색 모달창 닫기 */
@@ -22,6 +25,9 @@ export const useSearchMapModalController = () => {
   useEffect(() => {
     return () => {
       dispatch(postSlice.actions.resetSearchMapData());
+      if (isOpenSearchMapModal) {
+        closeSearchModalHandler();
+      }
     };
   }, []);
 
