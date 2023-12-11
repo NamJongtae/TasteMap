78c7cb36acac2c6a8dd15fef11a2937132eeb22e
MAIN6419 <dark9737@gmail.com>
2023-12-11 20:03:33 +0900

    Refactor: 프로필 업데이트를 낙관적 업데이트 처리 프로필 업데이트 후 UX 향상을 위해 수정 사항을 UI에 바로 적용되도록 낙관적 업데이트 처리 sucessCb 파라미터 값 제거 대신 필요 로직을 onSucess안에 직접 사용

--- src/hook/query/profile/useUpdateProfileMutation.ts
+++ src/hook/query/profile/useUpdateProfileMutation.ts
@@ -1,24 +1,118 @@
-import { useMutation, useQueryClient } from "@tanstack/react-query";
+import {
+  InfiniteData,
+  useMutation,
+  useQueryClient
+} from "@tanstack/react-query";
 import { updateMyProfile } from "../../../api/firebase/profileAPI";
-import { IProfileUpdateData } from "../../../api/apiType";
+import {
+  IMyProfileData,
+  IPostData,
+  IProfileUpdateData
+} from "../../../api/apiType";
 import { sweetToast } from "../../../library/sweetAlert/sweetAlert";
+import { useDispatch, useSelector } from "react-redux";
+import { AppDispatch, RootState } from "../../../store/store";
+import { DocumentData, QuerySnapshot } from "firebase/firestore";
+import { userSlice } from '../../../slice/userSlice';
 
-export const useUpdateProfileMutation = (sucessCb: () => void) => {
-  const queryClinet = useQueryClient();
+type InfinitePostsType = {
+  postDocs: QuerySnapshot<DocumentData, DocumentData>;
+  data: IPostData[];
+};
+
+export const useUpdateProfileMutation = () => {
+  const queryClient = useQueryClient();
+  const myInfo = useSelector((state: RootState) => state.user.myInfo);
+  const dispatch = useDispatch<AppDispatch>();
+
   const { mutate, isPending } = useMutation({
     mutationFn: (updateProfileData: IProfileUpdateData) =>
       updateMyProfile(updateProfileData),
-    onSuccess: ()=> {
-      queryClinet.invalidateQueries({ queryKey: ["profile", "my"] });
-      queryClinet.invalidateQueries({ queryKey: ["auth"] });
-      sucessCb();
-    },
-    onError: (error) => {
-      sweetToast(
-        "알 수 없는 에러가 발생하였습니다.\n잠시 후 다시 시도해 주세요.",
-        "warning"
+    onMutate: async (updateProfileData) => {
+      const img = updateProfileData.file;
+      let imgURL: string = "";
+      if (typeof img !== "string") {
+        imgURL = URL.createObjectURL(img);
+      }
+
+      await queryClient.cancelQueries({
+        queryKey: ["profile", "my"]
+      });
+
+      await queryClient.cancelQueries({
+        queryKey: ["posts", "PROFILE", myInfo.uid]
+      });
+
+      const previousMyProfile: IMyProfileData | undefined =
+        await queryClient.getQueryData(["profile", "my"]);
+
+      const previousPosts:
+        | InfiniteData<InfinitePostsType, unknown>
+        | undefined = await queryClient.getQueryData([
+        "posts",
+        "PROFILE",
+        myInfo.uid
+      ]);
+
+      const updateMyProfile = {
+        ...previousMyProfile,
+        photoURL: imgURL || previousMyProfile?.photoURL,
+        displayName:
+          updateProfileData.displayName || previousMyProfile?.displayName,
+        introduce: updateProfileData.introduce || previousMyProfile?.introduce
+      };
+
+      const updatePosts = previousPosts?.pages.map((page) => {
+        return {
+          ...page,
+          data: page.data.map((post) => {
+            return {
+              ...post,
+              displayName: updateProfileData.displayName,
+              photoURL: imgURL || previousMyProfile!.photoURL
+            };
+          })
+        };
+      });
+
+      queryClient.setQueryData(["profile", "my"], updateMyProfile);
+      queryClient.setQueryData(
+        ["posts", "PROFILE", myInfo.uid],
+        (data: InfiniteData<InfinitePostsType, unknown>) => ({
+          ...data,
+          pages: updatePosts
+        })
       );
-      console.log(error);
+
+      return { previousMyProfile, previousPosts };
+    },
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["profile", "my"],
+        refetchType: "inactive"
+      });
+      queryClient.invalidateQueries({
+        queryKey: ["posts", "PROFILE", myInfo.uid],
+        refetchType: "inactive"
+      });
+      queryClient.invalidateQueries({ queryKey: ["auth"] });
+      dispatch(userSlice.actions.setIsOpenUpdateProfileModal(false));
+    },
+    onError: (error, data, ctx) => {
+      if (ctx) {
+        queryClient.setQueryData(["posts", "HOME"], ctx.previousMyProfile);
+        queryClient.setQueryData(
+          ["posts", "PROFILE", myInfo.uid],
+          ctx.previousPosts
+        );
+      }
+      if (error) {
+        sweetToast(
+          "알 수 없는 에러가 발생하였습니다.\n잠시 후 다시 시도해 주세요.",
+          "warning"
+        );
+        console.log(error);
+      }
     }
   });
 
