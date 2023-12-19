9ab6df200492fe51112dd6d4c0e7d591ca798fdf
MAIN6419 <dark9737@gmail.com>
2023-12-19 22:53:24 +0900

    Feat: 회원가입 및 프로필 수정 IntroduceField에 useResizeTextArea customhook 적용

--- src/pages/profilePage/profileUpdateModal/profileUpdateForm/FormContent/IntroduceField/IntroduceField.tsx
+++ src/pages/profilePage/profileUpdateModal/profileUpdateForm/FormContent/IntroduceField/IntroduceField.tsx
@@ -1,43 +1,23 @@
-import React, { useEffect } from "react";
+import React from "react";
 import TextAreaField from "../../../../../../component/commons/UI/textAreaField/TextAreaField";
-import { useFormContext } from "react-hook-form";
+import { useResizeTextArea } from "../../../../../../hook/useResizeTextArea";
 interface IProps {
   introduceRef: React.RefObject<HTMLTextAreaElement>;
 }
 export default function IntroduceField({ introduceRef }: IProps) {
-  const { watch } = useFormContext();
-  const value = watch("introduce");
 
-  const resizeTextAreaHeight = () => {
-    if (introduceRef.current) {
-      introduceRef.current.style.height = "auto";
-      introduceRef.current.style.height =
-        introduceRef.current.scrollHeight - 20 + "px";
-    }
-  };
-
-  // 텍스트가 없는 경우 엔터키를 막음 빈 문자열 전송을 제한
-  const preventKeydownEnter = (e: React.KeyboardEvent<HTMLTextAreaElement>) => {
-    if (!e.currentTarget.value && e.key === "Enter") {
-      e.preventDefault();
-      return;
-    }
-  };
-
-  useEffect(() => {
-    resizeTextAreaHeight();
-  }, [value, resizeTextAreaHeight]);
-
+  useResizeTextArea({ name: "introduce", ref: introduceRef });
+  
   return (
     <TextAreaField
       label={"자기소개"}
       id={"introduce"}
       name={"introduce"}
-      onKeyDown={preventKeydownEnter}
       textareaRef={introduceRef}
       placeholder={"최대 100자까지 작성 가능합니다."}
       maxLength={100}
-      textareaStyle={{ backgroundColor: "#fff" }}
+      rows={1}
+      textareaStyle={{ backgroundColor: "#fff", maxHeight: "100px" }}
       required={false}
     />
   );
